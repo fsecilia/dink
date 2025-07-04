ef161f4cc6462a172bf232b767db37d454bf622a
Frank Secilia <frank.secilia@gmail.com>
2025-07-04 08:12:48 -0400

    unit test: add walking skeleton

---
+++ src/dink/unit_test.cpp
@@ -0,0 +1,15 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace dink {
+
+using namespace testing;
+
+TEST(test_suite, test_case) { ASSERT_TRUE(true); }
+
+} // namespace dink
