bb21d43318501ed7aacba0aeb8825149d02dd74c
Frank Secilia <frank.secilia@gmail.com>
2025-07-05 12:38:41 -0400

    add portable no_unique_address attribute

--- src/dink/dink.hpp
+++ src/dink/dink.hpp
@@ -6,4 +6,19 @@
 #pragma once
 
 namespace dink {
+
+// determine how to express no_unique_address
+// clang-format off
+#if defined __has_cpp_attribute
+    #if __has_cpp_attribute(msvc::no_unique_address)
+        #define dink_no_unique_address msvc::no_unique_address
+    #elif __has_cpp_attribute(no_unique_address)
+        #define dink_no_unique_address no_unique_address
+    #endif
+#endif
+#if !defined dink_no_unique_address
+    #define dink_no_unique_address
+#endif
+// clang-format on
+
 } // namespace dink
