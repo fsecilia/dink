5d1c41ac6e131ce6820f9926696fc3b6a667ca88
Frank Secilia <frank.secilia@gmail.com>
2025-07-05 14:31:51 -0400

    add arg_t

---
+++ src/dink/arg.hpp
@@ -0,0 +1,55 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#pragma once
+
+#include <dink/dink.hpp>
+#include <type_traits>
+
+namespace dink {
+
+/*!
+    filters matches that would eventually call a copy or move special member function.
+
+    If the composer were to try to resolve a copy or move ctor, it would recurse indefinitely and crash. This ignores
+    cases where there is 1 deduced arg and its type is the same as the resolved type, mod cv-ref.
+*/
+template <typename deduced_t, typename resolved_t, int num_args>
+concept smf_filter = num_args == 1 && std::is_same_v<std::remove_cvref_t<deduced_t>, resolved_t>;
+
+/*!
+    Resolves individual ctor args.
+
+    arg_t deduces the type of particular ctor arg and returns an instance resolved by a composer.
+
+    Deduction uses a pair of overloaded, implicit conversion templates to determine if it is matching a shared ref or a
+    transient value. It gets by on a technicality: the ref overload must be const. First, this overloads the conversion
+    templates unambiguously. If both were const or neither were const, this wouldn't compile. Second, when called from
+    a mutable arg_t to match a value type, the mutable value overload is a better match than the const ref overload.
+    Otherwise, the ref overload would always be chosen, even for value types.
+*/
+template <typename resolved_t, typename composer_t, int num_args>
+class arg_t
+{
+public:
+    template <smf_filter<resolved_t, num_args> deduced_t>
+    constexpr operator deduced_t()
+    {
+        return composer_.template resolve<deduced_t>();
+    }
+
+    template <smf_filter<resolved_t, num_args> deduced_t>
+    constexpr operator deduced_t&() const
+    {
+        return composer_.template resolve<deduced_t&>();
+    }
+
+    explicit constexpr arg_t(composer_t& composer) noexcept : composer_{composer} {}
+
+private:
+    composer_t& composer_;
+};
+
+} // namespace dink
---
+++ src/dink/arg_unit_test.cpp
@@ -0,0 +1,99 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#include <dink/arg.hpp>
+#include <dink/unit_test.hpp>
+
+namespace dink {
+namespace {
+
+struct arg_test_t : Test
+{
+    using id_t = int_t;
+    struct resolved_t
+    {
+        id_t id = 123;
+    };
+
+    struct handler_t
+    {
+        auto val(resolved_t resolved) const noexcept -> id_t { return id(resolved); }
+        auto lref(resolved_t& resolved) const noexcept -> id_t { return id(resolved); }
+        auto rref(resolved_t&& resolved) const noexcept -> id_t { return id(resolved); }
+        auto lcref(resolved_t const& resolved) const noexcept -> id_t { return id(resolved); }
+        auto rcref(resolved_t const&& resolved) const noexcept -> id_t { return id(resolved); }
+
+        auto id(resolved_t const& src) const noexcept -> id_t { return src.id; }
+    };
+
+    struct composer_t
+    {
+        template <typename resolved_t>
+        struct tag_t
+        {};
+
+        template <typename resolved_t>
+        auto resolve() -> decltype(auto)
+        {
+            return dispatch_resolve(tag_t<resolved_t>{});
+        }
+
+        auto dispatch_resolve(tag_t<resolved_t>) -> resolved_t { return resolve_val(); }
+        auto dispatch_resolve(tag_t<resolved_t&>) -> resolved_t& { return resolve_ref(); }
+        auto dispatch_resolve(tag_t<resolved_t&&>) -> resolved_t { return resolve_val(); }
+        auto dispatch_resolve(tag_t<resolved_t const>) -> resolved_t { return resolve_val(); }
+        auto dispatch_resolve(tag_t<resolved_t const&>) -> resolved_t& { return resolve_ref(); }
+        auto dispatch_resolve(tag_t<resolved_t const&&>) -> resolved_t { return resolve_val(); }
+
+        MOCK_METHOD(resolved_t, resolve_val, (), ());
+        MOCK_METHOD(resolved_t&, resolve_ref, (), ());
+
+        virtual ~composer_t() = default;
+    };
+
+    auto expect_ref() -> void { EXPECT_CALL(composer, resolve_ref()).WillOnce(ReturnRef(resolved)); }
+    auto expect_val() -> void { EXPECT_CALL(composer, resolve_val()).WillOnce(Return(resolved)); }
+
+    using sut_t = arg_t<resolved_t, composer_t, 1>;
+
+    id_t expected_id = 456;
+    resolved_t resolved{expected_id};
+    handler_t handler{};
+    StrictMock<composer_t> composer{};
+    sut_t sut{composer};
+};
+
+TEST_F(arg_test_t, val)
+{
+    expect_val();
+    ASSERT_EQ(expected_id, handler.val(sut));
+}
+
+TEST_F(arg_test_t, lref)
+{
+    expect_ref();
+    ASSERT_EQ(expected_id, handler.lref(sut));
+}
+
+TEST_F(arg_test_t, rref)
+{
+    expect_val();
+    ASSERT_EQ(expected_id, handler.rref(sut));
+}
+
+TEST_F(arg_test_t, lcref)
+{
+    expect_ref();
+    ASSERT_EQ(expected_id, handler.lcref(sut));
+}
+
+TEST_F(arg_test_t, rcref)
+{
+    expect_val();
+    ASSERT_EQ(expected_id, handler.rcref(sut));
+}
+
+} // namespace
+} // namespace dink
