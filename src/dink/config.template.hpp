9222225541df71d90decb4c6d397ff5d3dd04f37
Frank Secilia <frank.secilia@gmail.com>
2025-07-05 12:08:49 -0400

    cmake: gen config file

---
+++ src/dink/config.template.hpp
@@ -0,0 +1,12 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#pragma once
+
+#define dink_version_major ${dink_version_major}
+#define dink_version_minor ${dink_version_minor}
+#define dink_version_patch ${dink_version_patch}
+#define dink_version ${dink_version}
+#define dink_so_version ${dink_so_version}
