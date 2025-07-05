2c50414475f1f16c78549d1bdadeee47a3fe8650
Frank Secilia <frank.secilia@gmail.com>
2025-07-05 12:49:22 -0400

    add unit test header

--- src/dink/unit_test.cpp
+++ src/dink/unit_test.cpp
@@ -3,13 +3,13 @@
     Copyright (C) 2025 Frank Secilia
 */
 
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
+#include "unit_test.hpp"
 
 namespace dink {
 
-using namespace testing;
-
-TEST(test_suite, test_case) { ASSERT_TRUE(true); }
+TEST(test_suite, test_case)
+{
+    ASSERT_TRUE(true);
+}
 
 } // namespace dink
