604b8aabfbfd3502f3ce421fc423f3a58982801e
Frank Secilia <frank.secilia@gmail.com>
2025-07-06 06:00:20 -0400

    add type_map_t

---
+++ src/dink/type_map.hpp
@@ -0,0 +1,57 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#pragma once
+
+#include <dink/dink.hpp>
+
+namespace dink {
+
+/*!
+    Maps from deduced or requested types to resolved types.
+
+    type_map_t is a customization point to replace a requested type during resolution. Clients use it to specify which
+    concrete implementation should resolve an abstract type.
+*/
+template <typename requested_t>
+struct type_map_t
+{
+    using result_t = requested_t;
+};
+
+//! literal type found in type_map_t
+template <typename requested_t>
+using mapped_type_t = typename type_map_t<requested_t>::result_t;
+
+// ------------------------------------------------------------------------------------------------------------------ //
+
+/*
+    All requests go through the map, so type_map_t is also used to canonicalize refness and constness into either a
+    simple mutable value or a mutable ref.
+*/
+
+template <typename requested_t>
+struct type_map_t<requested_t&>
+{
+    using result_t = mapped_type_t<requested_t>&;
+};
+
+template <typename requested_t>
+struct type_map_t<requested_t&&> : type_map_t<requested_t>
+{};
+
+template <typename requested_t>
+struct type_map_t<requested_t const> : type_map_t<requested_t>
+{};
+
+template <typename requested_t>
+struct type_map_t<requested_t const&> : type_map_t<requested_t&>
+{};
+
+template <typename requested_t>
+struct type_map_t<requested_t const&&> : type_map_t<requested_t&&>
+{};
+
+} // namespace dink
---
+++ src/dink/type_map_unit_test.cpp
@@ -0,0 +1,51 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#include <dink/type_map.hpp>
+#include <dink/unit_test.hpp>
+#include <type_traits>
+
+namespace dink {
+namespace {
+
+struct requested_i
+{
+    virtual auto virtual_method() const noexcept -> void = 0;
+
+protected:
+    ~requested_i() noexcept = default;
+};
+
+struct resolved_t : public requested_i
+{
+    auto virtual_method() const noexcept -> void override {}
+};
+
+} // namespace
+
+template <>
+struct type_map_t<requested_i>
+{
+    using result_t = resolved_t;
+};
+
+namespace {
+
+static_assert(std::is_same_v<resolved_t, mapped_type_t<resolved_t>>);
+static_assert(std::is_same_v<resolved_t&, mapped_type_t<resolved_t&>>);
+static_assert(std::is_same_v<resolved_t, mapped_type_t<resolved_t&&>>);
+static_assert(std::is_same_v<resolved_t, mapped_type_t<resolved_t const>>);
+static_assert(std::is_same_v<resolved_t&, mapped_type_t<resolved_t const&>>);
+static_assert(std::is_same_v<resolved_t, mapped_type_t<resolved_t const&&>>);
+
+static_assert(std::is_same_v<resolved_t, mapped_type_t<requested_i>>);
+static_assert(std::is_same_v<resolved_t&, mapped_type_t<requested_i&>>);
+static_assert(std::is_same_v<resolved_t, mapped_type_t<requested_i&&>>);
+static_assert(std::is_same_v<resolved_t, mapped_type_t<requested_i const>>);
+static_assert(std::is_same_v<resolved_t&, mapped_type_t<requested_i const&>>);
+static_assert(std::is_same_v<resolved_t, mapped_type_t<requested_i const&&>>);
+
+} // namespace
+} // namespace dink
