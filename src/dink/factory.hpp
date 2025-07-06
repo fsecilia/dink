4f4c2eedc4878ec7ba4d8eb4f0668ca74783d385
Frank Secilia <frank.secilia@gmail.com>
2025-07-06 09:18:59 -0400

    add factory_t

---
+++ src/dink/factory.hpp
@@ -0,0 +1,100 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#pragma once
+
+#include <dink/dink.hpp>
+#include <type_traits>
+
+namespace dink {
+namespace factories {
+
+//! true if type has a static construct() method, regardless of signature.
+template <typename type_t>
+concept has_static_construct_method = requires {
+    { &std::remove_cvref_t<type_t>::construct };
+};
+
+//! Invokes resolved_t's ctor directly.
+template <typename resolved_t>
+class direct_ctor_t
+{
+public:
+    template <typename... args_t>
+    requires(std::is_constructible_v<resolved_t, args_t...>)
+    constexpr auto operator()(args_t&&... args) const -> resolved_t
+    {
+        return resolved_t{std::forward<args_t>(args)...};
+    }
+};
+
+//! Invokes static member resolved_t::construct.
+template <typename resolved_t>
+class static_construct_method_t;
+
+template <has_static_construct_method resolved_t>
+class static_construct_method_t<resolved_t>
+{
+public:
+    template <typename... args_t>
+    constexpr auto operator()(args_t&&... args) const -> resolved_t
+    {
+        return resolved_t::construct(std::forward<args_t>(args)...);
+    }
+};
+
+//! Invokes call operator on a factory supplied by the user.
+template <typename resolved_t, typename resolved_factory_t>
+class external_t
+{
+public:
+    template <typename... args_t>
+    requires(std::is_invocable_v<resolved_factory_t, resolved_factory_t&, args_t...>)
+    constexpr auto operator()(args_t&&... args) const -> resolved_t
+    {
+        return resolved_factory_(std::forward<args_t>(args)...);
+    }
+
+    explicit constexpr external_t(resolved_factory_t resolved_factory) noexcept
+        : resolved_factory_{std::move(resolved_factory)}
+    {}
+
+private:
+    resolved_factory_t resolved_factory_{};
+};
+
+/*!
+    Default factory if none is specified.
+
+    Chooses static_construct_method_t if resolved_t has a static construct() method, direct_ctor_t otherwise.
+*/
+template <typename resolved_t>
+struct default_t : public direct_ctor_t<resolved_t>
+{};
+
+template <has_static_construct_method resolved_t>
+struct default_t<resolved_t> : public static_construct_method_t<resolved_t>
+{};
+
+} // namespace factories
+
+/*!
+    Customization point to instantiate resolved_t.
+
+    The final instantiation of a resolved type happens in this type's call operator. By default, it first tries a
+    static "construct" method in resolved_t, then falls back to direct ctor invocation if nothing was found. Users can
+    specialize factory_t to override this behavior, or to use their own external factory.
+
+    To make customization easier, namespace factories has base types for common factory patterns. Users can derive a
+    specialized factory_t from these to choose a specific behavior.
+
+    During resolution, the instance of this factory is resolved by the composer. The factory type must be either
+    default constructible or resolvable.
+*/
+template <typename resolved_t>
+struct factory_t : factories::default_t<resolved_t>
+{};
+
+} // namespace dink
---
+++ src/dink/factory_unit_test.cpp
@@ -0,0 +1,299 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#include <dink/factory.hpp>
+#include <dink/unit_test.hpp>
+
+namespace dink {
+namespace factories {
+namespace {
+
+template <int_t position>
+struct param_t
+{
+    int_t index = position;
+};
+
+enum class source_t
+{
+    direct_ctor,
+    static_construct_method,
+    external
+};
+
+template <int_t num_params>
+struct resolved_t;
+
+template <>
+struct resolved_t<0>
+{
+    source_t source = source_t::direct_ctor;
+
+    static constexpr auto const expected_default_value = int_t{233};
+    int_t actual_default_value = expected_default_value;
+
+    resolved_t() noexcept = default;
+    resolved_t(param_t<0> param) noexcept;
+};
+
+template <>
+struct resolved_t<1>
+{
+    source_t source = source_t::direct_ctor;
+    param_t<0> param;
+
+    resolved_t(param_t<0> param) noexcept : param{param} {}
+    resolved_t(param_t<0>, param_t<1>) noexcept;
+};
+
+template <>
+struct resolved_t<2>
+{
+    source_t source = source_t::direct_ctor;
+    param_t<0> param_0;
+    param_t<1> param_1;
+
+    resolved_t(param_t<0> param_0, param_t<1> param_1) noexcept : param_0{param_0}, param_1{param_1} {}
+    resolved_t(param_t<0>, param_t<1>, param_t<2>) noexcept;
+};
+
+// ------------------------------------------------------------------------------------------------------------------ //
+
+template <int_t num_params>
+struct direct_ctor_resolved_t;
+
+template <>
+struct direct_ctor_resolved_t<0> : private resolved_t<0>
+{
+    using resolved_t<0>::resolved_t;
+    using resolved_t<0>::actual_default_value;
+    using resolved_t<0>::expected_default_value;
+    using resolved_t<0>::source;
+};
+
+template <>
+struct direct_ctor_resolved_t<1> : private resolved_t<1>
+{
+    using resolved_t<1>::resolved_t;
+    using resolved_t<1>::source;
+    using resolved_t<1>::param;
+};
+
+template <>
+struct direct_ctor_resolved_t<2> : private resolved_t<2>
+{
+    using resolved_t<2>::resolved_t;
+    using resolved_t<2>::source;
+    using resolved_t<2>::param_0;
+    using resolved_t<2>::param_1;
+};
+
+TEST(factory_test_t, direct_ctor_0)
+{
+    auto const actual = factory_t<direct_ctor_resolved_t<0>>{}();
+    ASSERT_EQ(source_t::direct_ctor, actual.source);
+    ASSERT_EQ(direct_ctor_resolved_t<0>::expected_default_value, actual.actual_default_value);
+}
+
+TEST(factory_test_t, direct_ctor_1)
+{
+    auto const expected = param_t<0>(3);
+    auto const actual = factory_t<direct_ctor_resolved_t<1>>{}(expected);
+    ASSERT_EQ(source_t::direct_ctor, actual.source);
+    ASSERT_EQ(expected.index, actual.param.index);
+}
+
+TEST(factory_test_t, direct_ctor_2)
+{
+    auto const expected_0 = param_t<0>(3);
+    auto const expected_1 = param_t<1>(5);
+    auto const actual = factory_t<direct_ctor_resolved_t<2>>{}(expected_0, expected_1);
+    ASSERT_EQ(source_t::direct_ctor, actual.source);
+    ASSERT_EQ(expected_0.index, actual.param_0.index);
+    ASSERT_EQ(expected_1.index, actual.param_1.index);
+}
+
+// ------------------------------------------------------------------------------------------------------------------ //
+
+template <int_t num_params>
+struct static_construct_method_resolved_t;
+
+template <>
+struct static_construct_method_resolved_t<0> : private direct_ctor_resolved_t<0>
+{
+    using direct_ctor_resolved_t<0>::source;
+    using direct_ctor_resolved_t<0>::actual_default_value;
+    using direct_ctor_resolved_t<0>::expected_default_value;
+
+    static auto construct() noexcept -> static_construct_method_resolved_t
+    {
+        auto result = static_construct_method_resolved_t{};
+        result.source = source_t::static_construct_method;
+        return result;
+    }
+
+private:
+    using direct_ctor_resolved_t<0>::direct_ctor_resolved_t;
+};
+static_assert(has_static_construct_method<static_construct_method_resolved_t<0>>);
+
+template <>
+struct static_construct_method_resolved_t<1> : private direct_ctor_resolved_t<1>
+{
+    using direct_ctor_resolved_t<1>::source;
+    using direct_ctor_resolved_t<1>::param;
+
+    static auto construct(param_t<0> param) noexcept -> static_construct_method_resolved_t
+    {
+        auto result = static_construct_method_resolved_t{param};
+        result.source = source_t::static_construct_method;
+        return result;
+    }
+
+private:
+    using direct_ctor_resolved_t<1>::direct_ctor_resolved_t;
+};
+
+template <>
+struct static_construct_method_resolved_t<2> : private direct_ctor_resolved_t<2>
+{
+    using direct_ctor_resolved_t<2>::source;
+    using direct_ctor_resolved_t<2>::param_0;
+    using direct_ctor_resolved_t<2>::param_1;
+
+    static auto construct(param_t<0> param_0, param_t<1> param_1) noexcept -> static_construct_method_resolved_t
+    {
+        auto result = static_construct_method_resolved_t{param_0, param_1};
+        result.source = source_t::static_construct_method;
+        return result;
+    }
+
+private:
+    using direct_ctor_resolved_t<2>::direct_ctor_resolved_t;
+};
+
+TEST(factory_test_t, static_construct_method_0)
+{
+    auto const actual = factory_t<static_construct_method_resolved_t<0>>{}();
+    ASSERT_EQ(source_t::static_construct_method, actual.source);
+    ASSERT_EQ(static_construct_method_resolved_t<0>::expected_default_value, actual.actual_default_value);
+}
+
+TEST(factory_test_t, static_construct_method_1)
+{
+    auto const expected = param_t<0>(3);
+    auto const actual = factory_t<static_construct_method_resolved_t<1>>{}(expected);
+    ASSERT_EQ(source_t::static_construct_method, actual.source);
+    ASSERT_EQ(expected.index, actual.param.index);
+}
+
+TEST(factory_test_t, static_construct_method_2)
+{
+    auto const expected_0 = param_t<0>(3);
+    auto const expected_1 = param_t<1>(5);
+    auto const actual = factory_t<static_construct_method_resolved_t<2>>{}(expected_0, expected_1);
+    ASSERT_EQ(source_t::static_construct_method, actual.source);
+    ASSERT_EQ(expected_0.index, actual.param_0.index);
+    ASSERT_EQ(expected_1.index, actual.param_1.index);
+}
+
+// ------------------------------------------------------------------------------------------------------------------ //
+
+template <int_t num_params>
+struct external_resolved_t;
+
+template <int_t num_params>
+struct external_resolved_factory_t
+{
+    template <typename... args_t>
+    auto operator()(args_t&&... args) const noexcept -> external_resolved_t<num_params>
+    {
+        auto result = external_resolved_t<num_params>{std::forward<args_t>(args)...};
+        result.source = source_t::external;
+        return result;
+    }
+};
+
+template <>
+struct external_resolved_t<0> : private static_construct_method_resolved_t<0>
+{
+    using static_construct_method_resolved_t<0>::source;
+    using static_construct_method_resolved_t<0>::actual_default_value;
+    using static_construct_method_resolved_t<0>::expected_default_value;
+
+private:
+    friend external_resolved_factory_t<0>;
+    using static_construct_method_resolved_t<0>::static_construct_method_resolved_t;
+};
+
+template <>
+struct external_resolved_t<1> : private static_construct_method_resolved_t<1>
+{
+    using static_construct_method_resolved_t<1>::source;
+    using static_construct_method_resolved_t<1>::param;
+
+private:
+    friend external_resolved_factory_t<1>;
+    using static_construct_method_resolved_t<1>::static_construct_method_resolved_t;
+};
+
+template <>
+struct external_resolved_t<2> : private static_construct_method_resolved_t<2>
+{
+    using static_construct_method_resolved_t<2>::source;
+    using static_construct_method_resolved_t<2>::param_0;
+    using static_construct_method_resolved_t<2>::param_1;
+
+private:
+    friend external_resolved_factory_t<2>;
+    using static_construct_method_resolved_t<2>::static_construct_method_resolved_t;
+};
+
+} // namespace
+} // namespace factories
+
+template <int_t num_params>
+struct factory_t<factories::external_resolved_t<num_params>>
+    : factories::external_t<
+          factories::external_resolved_t<num_params>, factories::external_resolved_factory_t<num_params>>
+{
+    factory_t()
+        : factories::external_t<
+              factories::external_resolved_t<num_params>,
+              factories::external_resolved_factory_t<num_params>>{factories::external_resolved_factory_t<num_params>{}}
+    {}
+};
+
+namespace factories {
+namespace {
+
+TEST(factory_test_t, external_0)
+{
+    auto const actual = factory_t<external_resolved_t<0>>{}();
+    ASSERT_EQ(source_t::external, actual.source);
+    ASSERT_EQ(external_resolved_t<0>::expected_default_value, actual.actual_default_value);
+}
+
+TEST(factory_test_t, external_1)
+{
+    auto const expected = param_t<0>(3);
+    auto const actual = factory_t<external_resolved_t<1>>{}(expected);
+    ASSERT_EQ(source_t::external, actual.source);
+    ASSERT_EQ(expected.index, actual.param.index);
+}
+
+TEST(factory_test_t, external_2)
+{
+    auto const expected_0 = param_t<0>(3);
+    auto const expected_1 = param_t<1>(5);
+    auto const actual = factory_t<external_resolved_t<2>>{}(expected_0, expected_1);
+    ASSERT_EQ(source_t::external, actual.source);
+    ASSERT_EQ(expected_0.index, actual.param_0.index);
+    ASSERT_EQ(expected_1.index, actual.param_1.index);
+}
+
+} // namespace
+} // namespace factories
+} // namespace dink
