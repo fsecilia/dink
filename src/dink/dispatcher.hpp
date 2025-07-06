53a13f0ea75425238fa9787a62b6973cbd0c265d
Frank Secilia <frank.secilia@gmail.com>
2025-07-06 17:38:04 -0400

    add dispatcher_t

---
+++ src/dink/dispatcher.hpp
@@ -0,0 +1,69 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#pragma once
+
+#include <dink/dink.hpp>
+#include <type_traits>
+
+namespace dink {
+
+/*!
+    max number of params dispatcher will try to deduce before erroring out
+
+    This value is mostly arbitrary, just higher than the number of parameters likely in generated code.
+*/
+constexpr auto const max_deduced_params = 3;
+
+//! true when invoking factory with args produces a result convertible to resolved_t
+template <typename resolved_t, typename factory_t, typename... args_t>
+concept resolvable = requires(factory_t factory, args_t... args) {
+    { factory(args...) } -> std::convertible_to<resolved_t>;
+};
+
+/*!
+    dispatches resolve() to the invocation of a factory that succeeds with the fewest arguments
+
+    dispatcher_t tries to invoke the given factory with an increasing number of arguments, starting from 0. The first
+    invocation that succeeds is chosen. This choice is made at compile time using type recursion.
+*/
+template <
+    typename resolved_t, typename composer_t, typename factory_t, template <typename, typename, int_t> class arg_t,
+    typename... args_t>
+struct dispatcher_t;
+
+//! successful dispatcher resolves using factory
+template <
+    typename resolved_t, typename composer_t, typename factory_t, template <typename, typename, int_t> class arg_t,
+    typename... args_t>
+requires(resolvable<resolved_t, factory_t, args_t...>)
+struct dispatcher_t<resolved_t, composer_t, factory_t, arg_t, args_t...>
+{
+public:
+    constexpr auto operator()(composer_t& composer) const -> resolved_t
+    {
+        auto factory = resolve_factory(composer);
+        return factory(args_t{composer}...);
+    }
+
+private:
+    constexpr auto resolve_factory(composer_t& composer) const -> factory_t
+    {
+        if constexpr (std::is_default_constructible_v<factory_t>) { return factory_t{}; }
+        else { return composer.template resolve<factory_t>(); }
+    }
+};
+
+//! unsuccessful dispatcher tries the next in the chain
+template <
+    typename resolved_t, typename composer_t, typename factory_t, template <typename, typename, int_t> class arg_t,
+    typename... args_t>
+requires(!resolvable<resolved_t, factory_t, args_t...> && sizeof...(args_t) <= max_deduced_params)
+struct dispatcher_t<resolved_t, composer_t, factory_t, arg_t, args_t...>
+    : dispatcher_t<
+          resolved_t, composer_t, factory_t, arg_t, args_t..., arg_t<resolved_t, composer_t, sizeof...(args_t) + 1>>
+{};
+
+} // namespace dink
---
+++ src/dink/dispatcher_unit_test.cpp
@@ -0,0 +1,94 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#include <compare>
+#include <dink/dispatcher.hpp>
+#include <dink/unit_test.hpp>
+
+namespace dink {
+namespace {
+
+struct dispatcher_test_t : Test
+{
+    struct composer_t
+    {};
+
+    template <typename resolved_t, typename composer_t, int_t num_args>
+    struct arg_t
+    {
+        composer_t const* composer = nullptr;
+        operator composer_t const*() const noexcept { return composer; }
+        operator int_t() const noexcept { return reinterpret_cast<int_t>(composer); }
+        explicit arg_t(composer_t& composer) noexcept : composer{&composer} {}
+    };
+
+    template <typename resolved_t>
+    struct factory_t
+    {
+        static_assert(!std::is_aggregate_v<resolved_t>);
+
+        template <typename... args_t>
+        requires(std::is_constructible_v<resolved_t, args_t...>)
+        constexpr auto operator()(args_t&&... args) const -> resolved_t
+        {
+            return resolved_t{std::forward<args_t>(args)...};
+        }
+    };
+
+    composer_t composer{};
+};
+
+TEST_F(dispatcher_test_t, resolve_0_arg)
+{
+    constexpr auto const expected_default_value = int_t{3};
+    struct resolved_t
+    {
+        int_t actual_default_value{expected_default_value};
+        resolved_t() = default;
+    };
+
+    using sut_t = dispatcher_t<resolved_t, composer_t, factory_t<resolved_t>, arg_t>;
+    sut_t sut{};
+
+    auto const resolved = sut(composer);
+    ASSERT_EQ(expected_default_value, resolved.actual_default_value);
+}
+
+TEST_F(dispatcher_test_t, resolve_1_arg)
+{
+    struct resolved_t
+    {
+        int_t composer;
+
+        resolved_t(int_t composer) : composer{composer} {}
+    };
+
+    using sut_t = dispatcher_t<resolved_t, composer_t, factory_t<resolved_t>, arg_t>;
+    sut_t sut{};
+
+    auto const resolved = sut(composer);
+    ASSERT_EQ(reinterpret_cast<int_t>(&composer), resolved.composer);
+}
+
+TEST_F(dispatcher_test_t, resolve_2_arg)
+{
+    struct resolved_t
+    {
+        int_t composer0;
+        composer_t const* composer1;
+
+        resolved_t(int_t composer0, composer_t const* composer1) : composer0{composer0}, composer1{composer1} {}
+    };
+
+    using sut_t = dispatcher_t<resolved_t, composer_t, factory_t<resolved_t>, arg_t>;
+    sut_t sut{};
+
+    auto const resolved = sut(composer);
+    ASSERT_EQ(reinterpret_cast<int_t>(&composer), resolved.composer0);
+    ASSERT_EQ(&composer, resolved.composer1);
+}
+
+} // namespace
+} // namespace dink
