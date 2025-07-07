c19138bd49f0e8093a5f41a1d692ac4573f196ee
Frank Secilia <frank.secilia@gmail.com>
2025-07-07 10:02:07 -0400

    add dink_t

---
+++ src/dink/dink.hpp
@@ -0,0 +1,21 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#pragma once
+
+#include <dink/lib.hpp>
+#include <dink/arg.hpp>
+#include <dink/composer.hpp>
+#include <dink/dispatcher.hpp>
+#include <dink/factory.hpp>
+#include <dink/resolver.hpp>
+#include <dink/type_map.hpp>
+
+namespace dink {
+
+class dink_t : public composer_t<resolvers::transient_t<dispatcher_t, factory_t, arg_t>, resolvers::shared_t>
+{};
+
+} // namespace dink
