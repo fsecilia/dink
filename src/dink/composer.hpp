d52fe7b3d34f7e714044135a7ba141e767439586
Frank Secilia <frank.secilia@gmail.com>
2025-07-07 09:59:13 -0400

    add composer_t

---
+++ src/dink/composer.hpp
@@ -0,0 +1,63 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#pragma once
+
+#include <dink/lib.hpp>
+#include <dink/type_map.hpp>
+#include <type_traits>
+
+namespace dink {
+
+template <typename requested_t> concept shared = std::is_lvalue_reference_v<requested_t>;
+template <typename requested_t> concept transient = !shared<requested_t>;
+
+/*!
+    composes object graphs recursively
+
+    Composer creates object graphs. It deduces the parameters necessary to construct a requested type, constructs them,
+    then uses them to construct the requested type. Construction recurses through each parameter, eventually creating
+    the entire dependency tree.
+
+    Composer has one function template, resolve, that returns an instance of the requested type. If the requested type
+    is an lvalue reference, the result is created once and shared, otherwise, new instances are created as requested.
+    The construction method is configurable per type. The instance may be constructed directly, through a static
+    factory method, or by an external factory supplied by the user. The number and types of arguments required to call
+    the construction method is determined automatically. If the construction method is overloaded, the shortest
+    overload is chosen. Each argument is itself resolved by the composer before being passed to the construction
+    method, and this recurses until the graph necessary to create an instance is complete.
+
+              +---------------------------------------------------------+
+              |                                                         |
+              |       +-->transient<>-->dispatcher<>-->factory<>-->arg--+
+              v       |                                   ^
+          composer<>--+                                   T
+              ^       |                        +----------+----------+
+              |       +--->shared--+           |          |          |
+              |                    |        static     direct     external<>-->resolved_factory
+              +--------------------+
+*/
+template <typename transient_resolver_t, typename shared_resolver_t>
+class composer_t
+{
+public:
+    template <transient requested_t>
+    constexpr auto resolve() -> mapped_type_t<requested_t>
+    {
+        return transient_resolver_.template resolve<mapped_type_t<requested_t>>(*this);
+    }
+
+    template <shared requested_t>
+    constexpr auto resolve() -> mapped_type_t<requested_t>&
+    {
+        return shared_resolver_.template resolve<mapped_type_t<requested_t>>(*this);
+    }
+
+private:
+    transient_resolver_t transient_resolver_{};
+    shared_resolver_t shared_resolver_{};
+};
+
+} // namespace dink
---
+++ src/dink/composer_unit_test.cpp
@@ -0,0 +1,57 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#include <dink/unit_test.hpp>
+#include <dink/composer.hpp>
+
+namespace dink {
+namespace {
+
+struct composer_test_t : Test
+{
+    struct resolved_t
+    {
+        int_t result;
+    };
+
+    struct transient_resolver_t
+    {
+        static constexpr resolved_t const expected_result{.result = 3};
+
+        template <typename resolved_t, typename composer_t>
+        auto resolve(composer_t& composer) const -> resolved_t
+        {
+            return expected_result;
+        }
+    };
+
+    struct shared_resolver_t
+    {
+        inline static resolved_t expected_result{.result = 5};
+
+        template <typename resolved_t, typename composer_t>
+        auto resolve(composer_t& composer) const -> resolved_t&
+        {
+            return expected_result;
+        }
+    };
+
+    using sut_t = composer_t<transient_resolver_t, shared_resolver_t>;
+
+    sut_t sut{};
+};
+
+TEST_F(composer_test_t, resolve_transient)
+{
+    ASSERT_EQ(transient_resolver_t::expected_result.result, sut.template resolve<resolved_t>().result);
+}
+
+TEST_F(composer_test_t, resolve_shared)
+{
+    ASSERT_EQ(&shared_resolver_t::expected_result, &sut.template resolve<resolved_t&>());
+}
+
+} // namespace
+} // namespace dink
