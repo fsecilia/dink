7379ff77fc8b949364f1e769d633e1cca64c1cbb
Frank Secilia <frank.secilia@gmail.com>
2025-07-08 05:51:51 -0400

    stop clang-formatting cmake config

--- src/dink/config.template.hpp
+++ src/dink/config.template.hpp
@@ -5,8 +5,12 @@
 
 #pragma once
 
+// clang-format off
+
 #define dink_version_major ${dink_version_major}
 #define dink_version_minor ${dink_version_minor}
 #define dink_version_patch ${dink_version_patch}
 #define dink_version ${dink_version}
 #define dink_so_version ${dink_so_version}
+
+// clang-format on
