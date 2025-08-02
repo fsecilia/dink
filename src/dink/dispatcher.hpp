8d0dc5b4c9ced97d76839fe19c0b054d04ddacf1
Frank Secilia <frank.secilia@gmail.com>
2025-08-02 09:00:03 -0400

    dispatcher: list one reason why resolve may fail
    This is a trivial documentation update to describe a scenario that comes
    up occasionally that is difficult to diagnose if you don't know why, but
    trivial once the possibility is seen.
    
    The reason is a base type defines static construct(), but a leaf does
    not, but now this commit message is starting to duplicate the content,
    so see the comments added in the commit.

--- src/dink/dispatcher.hpp
+++ src/dink/dispatcher.hpp
@@ -65,6 +65,15 @@ struct dispatcher_t<resolved_t, composer_t, factory_t, arg_t, args_t...>
         If you've hit this, dispatcher_t could not match a signature when trying to invoke the factory for resolved_t.
         Some of the parameters may be ambiguous to arg_t, or the number of params may be larger than
         dink_max_deduced_params. Follow factory_resolvable into the factory to find out why.
+
+        One notable cause is when a leaf type does not define a static construct(), but at least one of its bases do.
+        has_static_construct_method cannot determine this case independently of defining it in the leaf, so it still
+        tries matching against static construct, which isn't available.
+
+        If this is the case, the current workaround is to define a construct method in the leaf and forward to its
+        ctor. This can become unweildy for deep hierarchies, but it works well enough in practice.
+
+        /sa has_static_construct_method 
     */
     static_assert(factory_resolvable<resolved_t, factory_t, args_t...>);
 };
