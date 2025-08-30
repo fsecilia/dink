7c1973333e9e4e83ced94bb2deb2204acf17cfcc
Frank Secilia <frank.secilia@gmail.com>
2025-08-30 16:30:39 -0400

    memory: bash cache line size
    This was original added to combat false sharing in the arena allocator, but that is just too low a level of implementation to worry about that. There isn't really anywhere in a di container to be concerned with false sharing directly, so out it comes.

--- src/dink/memory.hpp
+++ src/dink/memory.hpp
@@ -7,7 +7,6 @@
 
 #include <dink/lib.hpp>
 #include <cstddef>
-#include <new>
 #include <utility>
 
 // clang-format off
@@ -37,19 +36,6 @@ namespace memory {
 namespace fallback {
 
 /*!
-    estimates cache line size using stdlib
-
-    This implementation uses std::hardware_destructive_interference_size verbatim. This is the best estimation at
-    compile time. It is fixed at compile time, though, so it will underestimate when running the same compiled binary
-    on more recent hardware with a larger cache.
-*/
-class cache_line_size_t
-{
-public:
-    auto operator()() const noexcept -> std::size_t { return std::hardware_destructive_interference_size; }
-};
-
-/*!
     reasonable estimate of current os page sizes
 
     Page size does not have a stdlib constant, so we default to 4k.
@@ -63,34 +49,12 @@ public:
 } // namespace fallback
 
 #if groundwork_memory_impl == groundwork_memory_impl_fallback
-using cache_line_size_t = fallback::cache_line_size_t;
 using page_size_size_t = fallback::page_size_t;
 #endif
 
 #if groundwork_memory_impl == groundwork_memory_impl_posix
 
 namespace posix {
-
-//! gets level 1 data cache line size directly from sysconf
-template <typename api_t, typename fallback_t>
-class cache_line_size_t
-{
-public:
-    static constexpr auto const sysconf_cache_line_size_name = _SC_LEVEL1_DCACHE_LINESIZE;
-
-    auto operator()() const noexcept -> std::size_t
-    {
-        auto const result = api_.sysconf(sysconf_cache_line_size_name);
-        if (result > 0) return static_cast<size_t>(result);
-        return fallback_();
-    }
-
-    cache_line_size_t(api_t api, fallback_t fallback) noexcept : api_{std::move(api)}, fallback_{std::move(fallback)} {}
-
-private:
-    [[no_unique_address]] api_t api_;
-    [[no_unique_address]] fallback_t fallback_;
-};
 
 //! gets page size directly from sysconf
 template <typename api_t, typename fallback_t>
@@ -121,7 +85,6 @@ struct api_t
 } // namespace posix
 
 using api_t = posix::api_t;
-using cache_line_size_t = posix::cache_line_size_t<api_t, fallback::cache_line_size_t>;
 using page_size_t = posix::page_size_t<api_t, fallback::page_size_t>;
 
 #endif
--- src/dink/memory_unit_test.cpp
+++ src/dink/memory_unit_test.cpp
@@ -17,11 +17,6 @@ struct memory_fallback_test_t : Test
     }
 };
 
-TEST_F(memory_fallback_test_t, cache_line_size_is_power_of_two)
-{
-    ASSERT_TRUE(is_nonzero_power_of_two(fallback::cache_line_size_t{}()));
-}
-
 TEST_F(memory_fallback_test_t, page_size_is_power_of_two)
 {
     ASSERT_TRUE(is_nonzero_power_of_two(fallback::page_size_t{}()));
@@ -30,61 +25,6 @@ TEST_F(memory_fallback_test_t, page_size_is_power_of_two)
 // ---------------------------------------------------------------------------------------------------------------------
 
 #if groundwork_memory_impl == groundwork_memory_impl_posix
-
-struct memory_posix_test_cache_line_size_t : Test
-{
-    struct mock_api_t
-    {
-        MOCK_METHOD(long int, sysconf, (int name), (const, noexcept));
-        virtual ~mock_api_t() = default;
-    };
-    StrictMock<mock_api_t> mock_api;
-
-    struct api_t
-    {
-        auto sysconf(int name) const noexcept -> long int { return mock->sysconf(name); }
-        mock_api_t* mock = nullptr;
-    };
-    api_t api{&mock_api};
-
-    struct mock_fallback_t
-    {
-        MOCK_METHOD(std::size_t, call_operator, (), (const, noexcept));
-        virtual ~mock_fallback_t() = default;
-    };
-    StrictMock<mock_fallback_t> mock_fallback;
-
-    struct fallback_t
-    {
-        auto operator()() const noexcept -> std::size_t { return mock->call_operator(); }
-        mock_fallback_t* mock = nullptr;
-    };
-    fallback_t fallback{&mock_fallback};
-
-    using sut_t = posix::cache_line_size_t<api_t, fallback_t>;
-    sut_t sut{api, fallback};
-};
-
-TEST_F(memory_posix_test_cache_line_size_t, succeed)
-{
-    auto const expected = std::size_t{128};
-    EXPECT_CALL(mock_api, sysconf(sut_t::sysconf_cache_line_size_name)).WillOnce(Return(static_cast<int>(expected)));
-
-    auto const actual = sut();
-    ASSERT_EQ(expected, actual);
-}
-
-TEST_F(memory_posix_test_cache_line_size_t, failed_sysconf_uses_fallback)
-{
-    auto const expected = std::size_t{64};
-    EXPECT_CALL(mock_api, sysconf(sut_t::sysconf_cache_line_size_name)).WillOnce(Return(static_cast<int>(0)));
-    EXPECT_CALL(mock_fallback, call_operator()).WillOnce(Return(expected));
-
-    auto const actual = sut();
-    ASSERT_EQ(expected, actual);
-}
-
-// ---------------------------------------------------------------------------------------------------------------------
 
 struct memory_posix_test_page_size_t : Test
 {
