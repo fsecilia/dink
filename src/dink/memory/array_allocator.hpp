3cdd8d0bda88216d34a326a826f919eb64678fe4
Frank Secilia <frank.secilia@gmail.com>
2025-08-30 21:04:29 -0400

    memory: add array allocator

---
+++ src/dink/memory/array_allocator.hpp
@@ -0,0 +1,32 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#pragma once
+
+#include <dink/lib.hpp>
+#include <cstddef>
+#include <memory>
+#include <new>
+
+namespace dink {
+
+//! const callable that takes size and alignment, then returns unique_ptr<byte[]>
+template <typename array_allocator_t>
+concept array_allocator = requires(array_allocator_t const& array_allocator, size_t size, std::align_val_t alignment) {
+    { array_allocator(size, alignment) } -> std::same_as<std::unique_ptr<std::byte[]>>;
+};
+
+//! allocates arrays using new
+class array_allocator_t
+{
+public:
+    using dynamic_array_t = std::unique_ptr<std::byte[]>;
+    auto operator()(std::size_t size, std::align_val_t alignment) const -> dynamic_array_t
+    {
+        return dynamic_array_t{new (alignment) std::byte[size]};
+    }
+};
+
+} // namespace dink
