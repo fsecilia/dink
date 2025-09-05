eaae8723b631d0660919a8a1cba38ae8b0554338
Frank Secilia <frank.secilia@gmail.com>
2025-09-05 10:01:45 -0400

    tuple: simplify naming in test

--- src/dink/scope/config/detail/tuple.hpp
+++ src/dink/scope/config/detail/tuple.hpp
@@ -12,47 +12,50 @@
 namespace dink::detail::tuple {
 
 template <typename tuple_t, typename element_t>
-struct tuple_contains_f;
+struct contains_f;
 
 template <typename element_t, typename... elements_t>
-struct tuple_contains_f<std::tuple<elements_t...>, element_t> : std::disjunction<std::is_same<element_t, elements_t>...>
+struct contains_f<std::tuple<elements_t...>, element_t> : std::disjunction<std::is_same<element_t, elements_t>...>
 {};
 
+//! true if tuple_t has an element of type element_t
 template <typename tuple_t, typename element_t>
-inline constexpr auto const tuple_contains_v = tuple_contains_f<tuple_t, element_t>::value;
+inline constexpr auto const contains_v = contains_f<tuple_t, element_t>::value;
 
 template <typename tuple_t, typename element_t>
-struct tuple_append_f;
+struct append_f;
 
 template <typename element_t, typename... elements_t>
-struct tuple_append_f<std::tuple<elements_t...>, element_t>
+struct append_f<std::tuple<elements_t...>, element_t>
 {
     using type = std::tuple<elements_t..., element_t>;
 };
 
+//! appends element to tuple
 template <typename tuple_t, typename element_t>
-using tuple_append_t = tuple_append_f<tuple_t, element_t>::type;
+using append_t = append_f<tuple_t, element_t>::type;
 
 template <typename accumulator_tuple_t, typename remaining_tuple_t>
-struct tuple_unique_f;
+struct unique_f;
 
 template <typename accumulator_tuple_t>
-struct tuple_unique_f<accumulator_tuple_t, std::tuple<>>
+struct unique_f<accumulator_tuple_t, std::tuple<>>
 {
     using type = accumulator_tuple_t;
 };
 
-template <typename accumulator_tuple_t, typename next_t, typename... remaining_t>
-struct tuple_unique_f<accumulator_tuple_t, std::tuple<next_t, remaining_t...>>
+template <typename accumulator_tuple_t, typename next_element_t, typename... remaining_elements_t>
+struct unique_f<accumulator_tuple_t, std::tuple<next_element_t, remaining_elements_t...>>
 {
-    using type = tuple_unique_f<
+    using type = unique_f<
         std::conditional_t<
-            tuple_contains_v<accumulator_tuple_t, next_t>, accumulator_tuple_t,
-            tuple_append_t<accumulator_tuple_t, next_t>>,
-        std::tuple<remaining_t...>>::type;
+            contains_v<accumulator_tuple_t, next_element_t>, accumulator_tuple_t,
+            append_t<accumulator_tuple_t, next_element_t>>,
+        std::tuple<remaining_elements_t...>>::type;
 };
 
+//! removes repeated elements
 template <typename tuple_t>
-using tuple_unique_t = tuple_unique_f<std::tuple<>, tuple_t>::type;
+using unique_t = unique_f<std::tuple<>, tuple_t>::type;
 
 } // namespace dink::detail::tuple
