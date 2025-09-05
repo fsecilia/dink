445a9169b81c2d341ec785ee86bc72830bd50239
Frank Secilia <frank.secilia@gmail.com>
2025-09-05 14:35:22 -0400

    tuple: reimplement unique to use key projection
    Previously, I was going to use keys_t to extract the keys in a tuple, then work on the keys next to the elements. Instead, the keys can be extracted from head and accumulated. I toyed with using a projection metafunction to specify what to extract, but since this is coupled in the problem domain, it's ok to couple here, too.

--- src/dink/scope/config/detail/tuple.hpp
+++ src/dink/scope/config/detail/tuple.hpp
@@ -22,6 +22,8 @@ struct contains_f<std::tuple<elements_t...>, element_t> : std::disjunction<std::
 template <typename tuple_t, typename element_t>
 inline constexpr auto const contains_v = contains_f<tuple_t, element_t>::value;
 
+// ---------------------------------------------------------------------------------------------------------------------
+
 template <typename tuple_t, typename element_t>
 struct append_f;
 
@@ -35,27 +37,41 @@ struct append_f<std::tuple<elements_t...>, element_t>
 template <typename tuple_t, typename element_t>
 using append_t = append_f<tuple_t, element_t>::type;
 
-template <typename accumulator_tuple_t, typename remaining_tuple_t>
-struct unique_f;
+// ---------------------------------------------------------------------------------------------------------------------
 
-template <typename accumulator_tuple_t>
-struct unique_f<accumulator_tuple_t, std::tuple<>>
+template <typename accumulated_elements_tuple_t, typename accumulated_keys_tuple_t, typename remaining_elements_tuple_t>
+struct unique_by_key_f;
+
+template <typename accumulated_elements_tuple_t, typename accumulated_keys_tuple_t>
+struct unique_by_key_f<accumulated_elements_tuple_t, accumulated_keys_tuple_t, std::tuple<>>
 {
-    using type = accumulator_tuple_t;
+    using type = accumulated_elements_tuple_t;
 };
 
-template <typename accumulator_tuple_t, typename next_element_t, typename... remaining_elements_t>
-struct unique_f<accumulator_tuple_t, std::tuple<next_element_t, remaining_elements_t...>>
+template <
+    typename accumulated_elements_tuple_t, typename accumulated_keys_tuple_t, typename next_element_t,
+    typename... remaining_elements_t>
+struct unique_by_key_f<
+    accumulated_elements_tuple_t, accumulated_keys_tuple_t, std::tuple<next_element_t, remaining_elements_t...>>
 {
-    using type = unique_f<
-        std::conditional_t<
-            contains_v<accumulator_tuple_t, next_element_t>, accumulator_tuple_t,
-            append_t<accumulator_tuple_t, next_element_t>>,
-        std::tuple<remaining_elements_t...>>::type;
+private:
+    using key_type_t = next_element_t::key_t;
+    static constexpr bool is_duplicate = detail::tuple::contains_v<accumulated_keys_tuple_t, key_type_t>;
+
+    using next_accumulated_elements_t = std::conditional_t<
+        is_duplicate, accumulated_elements_tuple_t,
+        detail::tuple::append_t<accumulated_elements_tuple_t, next_element_t>>;
+
+    using next_accumulated_keys_t = std::conditional_t<
+        is_duplicate, accumulated_keys_tuple_t, detail::tuple::append_t<accumulated_keys_tuple_t, key_type_t>>;
+
+public:
+    using type = unique_by_key_f<
+        next_accumulated_elements_t, next_accumulated_keys_t, std::tuple<remaining_elements_t...>>::type;
 };
 
-//! removes repeated elements
+//! produces new tuple containing only first instance of each element containing same key_t; unique by projection of key
 template <typename tuple_t>
-using unique_t = unique_f<std::tuple<>, tuple_t>::type;
+using unique_by_key_t = unique_by_key_f<std::tuple<>, std::tuple<>, tuple_t>::type;
 
 } // namespace dink::detail::tuple
--- src/dink/scope/config/detail/tuple_unit_test.cpp
+++ src/dink/scope/config/detail/tuple_unit_test.cpp
@@ -9,6 +9,9 @@
 namespace dink::detail::tuple {
 namespace {
 
+template <typename... elements_t>
+using t = std::tuple<elements_t...>;
+
 template <int id>
 struct unique_value_t
 {};
@@ -16,9 +19,6 @@ struct unique_value_t
 using v0 = unique_value_t<0>;
 using v1 = unique_value_t<1>;
 using v2 = unique_value_t<2>;
-
-template <typename... elements_t>
-using t = std::tuple<elements_t...>;
 
 // clang-format off
 
@@ -194,46 +194,58 @@ static_assert(std::is_same_v<append_t<t<v2, v2, v0>, v2>, t<v2, v2, v0, v2>>);
 static_assert(std::is_same_v<append_t<t<v2, v2, v1>, v2>, t<v2, v2, v1, v2>>);
 static_assert(std::is_same_v<append_t<t<v2, v2, v2>, v2>, t<v2, v2, v2, v2>>);
 
-static_assert(std::is_same_v<unique_t<t<>>, t<>>);
-static_assert(std::is_same_v<unique_t<t<v0>>, t<v0>>);
-static_assert(std::is_same_v<unique_t<t<v1>>, t<v1>>);
-static_assert(std::is_same_v<unique_t<t<v2>>, t<v2>>);
-static_assert(std::is_same_v<unique_t<t<v0, v0>>, t<v0>>);
-static_assert(std::is_same_v<unique_t<t<v0, v1>>, t<v0, v1>>);
-static_assert(std::is_same_v<unique_t<t<v0, v2>>, t<v0, v2>>);
-static_assert(std::is_same_v<unique_t<t<v1, v0>>, t<v1, v0>>);
-static_assert(std::is_same_v<unique_t<t<v1, v1>>, t<v1>>);
-static_assert(std::is_same_v<unique_t<t<v1, v2>>, t<v1, v2>>);
-static_assert(std::is_same_v<unique_t<t<v2, v0>>, t<v2, v0>>);
-static_assert(std::is_same_v<unique_t<t<v2, v1>>, t<v2, v1>>);
-static_assert(std::is_same_v<unique_t<t<v2, v2>>, t<v2>>);
-static_assert(std::is_same_v<unique_t<t<v0, v0, v0>>, t<v0>>);
-static_assert(std::is_same_v<unique_t<t<v0, v0, v1>>, t<v0, v1>>);
-static_assert(std::is_same_v<unique_t<t<v0, v0, v2>>, t<v0, v2>>);
-static_assert(std::is_same_v<unique_t<t<v0, v1, v0>>, t<v0, v1>>);
-static_assert(std::is_same_v<unique_t<t<v0, v1, v1>>, t<v0, v1>>);
-static_assert(std::is_same_v<unique_t<t<v0, v1, v2>>, t<v0, v1, v2>>);
-static_assert(std::is_same_v<unique_t<t<v0, v2, v0>>, t<v0, v2>>);
-static_assert(std::is_same_v<unique_t<t<v0, v2, v1>>, t<v0, v2, v1>>);
-static_assert(std::is_same_v<unique_t<t<v0, v2, v2>>, t<v0, v2>>);
-static_assert(std::is_same_v<unique_t<t<v1, v0, v0>>, t<v1, v0>>);
-static_assert(std::is_same_v<unique_t<t<v1, v0, v1>>, t<v1, v0>>);
-static_assert(std::is_same_v<unique_t<t<v1, v0, v2>>, t<v1, v0, v2>>);
-static_assert(std::is_same_v<unique_t<t<v1, v1, v0>>, t<v1, v0>>);
-static_assert(std::is_same_v<unique_t<t<v1, v1, v1>>, t<v1>>);
-static_assert(std::is_same_v<unique_t<t<v1, v1, v2>>, t<v1, v2>>);
-static_assert(std::is_same_v<unique_t<t<v1, v2, v0>>, t<v1, v2, v0>>);
-static_assert(std::is_same_v<unique_t<t<v1, v2, v1>>, t<v1, v2>>);
-static_assert(std::is_same_v<unique_t<t<v1, v2, v2>>, t<v1, v2>>);
-static_assert(std::is_same_v<unique_t<t<v2, v0, v0>>, t<v2, v0>>);
-static_assert(std::is_same_v<unique_t<t<v2, v0, v1>>, t<v2, v0, v1>>);
-static_assert(std::is_same_v<unique_t<t<v2, v0, v2>>, t<v2, v0>>);
-static_assert(std::is_same_v<unique_t<t<v2, v1, v0>>, t<v2, v1, v0>>);
-static_assert(std::is_same_v<unique_t<t<v2, v1, v1>>, t<v2, v1>>);
-static_assert(std::is_same_v<unique_t<t<v2, v1, v2>>, t<v2, v1>>);
-static_assert(std::is_same_v<unique_t<t<v2, v2, v0>>, t<v2, v0>>);
-static_assert(std::is_same_v<unique_t<t<v2, v2, v1>>, t<v2, v1>>);
-static_assert(std::is_same_v<unique_t<t<v2, v2, v2>>, t<v2>>);
+template <typename key_p>
+struct keyed_value_t
+{
+    using key_t = key_p;
+};
+
+using k0 = keyed_value_t<unique_value_t<0>>;
+using k1 = keyed_value_t<unique_value_t<1>>;
+using k2 = keyed_value_t<unique_value_t<2>>;
+
+// exhaustive test of all combinations of elements containing keys with values 0, 1, 2 of lengths up to 3
+
+static_assert(std::is_same_v<unique_by_key_t<t<>>, t<>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k0>>, t<k0>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k1>>, t<k1>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k2>>, t<k2>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k0, k0>>, t<k0>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k0, k1>>, t<k0, k1>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k0, k2>>, t<k0, k2>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k1, k0>>, t<k1, k0>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k1, k1>>, t<k1>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k1, k2>>, t<k1, k2>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k2, k0>>, t<k2, k0>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k2, k1>>, t<k2, k1>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k2, k2>>, t<k2>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k0, k0, k0>>, t<k0>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k0, k0, k1>>, t<k0, k1>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k0, k0, k2>>, t<k0, k2>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k0, k1, k0>>, t<k0, k1>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k0, k1, k1>>, t<k0, k1>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k0, k1, k2>>, t<k0, k1, k2>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k0, k2, k0>>, t<k0, k2>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k0, k2, k1>>, t<k0, k2, k1>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k0, k2, k2>>, t<k0, k2>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k1, k0, k0>>, t<k1, k0>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k1, k0, k1>>, t<k1, k0>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k1, k0, k2>>, t<k1, k0, k2>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k1, k1, k0>>, t<k1, k0>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k1, k1, k1>>, t<k1>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k1, k1, k2>>, t<k1, k2>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k1, k2, k0>>, t<k1, k2, k0>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k1, k2, k1>>, t<k1, k2>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k1, k2, k2>>, t<k1, k2>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k2, k0, k0>>, t<k2, k0>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k2, k0, k1>>, t<k2, k0, k1>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k2, k0, k2>>, t<k2, k0>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k2, k1, k0>>, t<k2, k1, k0>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k2, k1, k1>>, t<k2, k1>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k2, k1, k2>>, t<k2, k1>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k2, k2, k0>>, t<k2, k0>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k2, k2, k1>>, t<k2, k1>>);
+static_assert(std::is_same_v<unique_by_key_t<t<k2, k2, k2>>, t<k2>>);
 
 // clang-format on
 
