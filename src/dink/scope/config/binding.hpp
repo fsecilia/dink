28aace503d147100ef4b981e751e381a6b968071
Frank Secilia <frank.secilia@gmail.com>
2025-09-05 14:38:34 -0400

    bindings: bash stale tuple key extraction
    unique_by_key_t does it all in one step now.

--- src/dink/scope/config/binding.hpp
+++ src/dink/scope/config/binding.hpp
@@ -11,17 +11,6 @@
 
 namespace dink {
 
-template <typename tuple_t>
-struct keys_f;
-
-template <typename... elements_t>
-struct keys_f<std::tuple<elements_t...>>
-{
-    using type = std::tuple<typename elements_t::key_t...>;
-};
-
-//! projects to new tuple containing only key_t member of each element
-template <typename tuple_t>
-using keys_t = typename keys_f<tuple_t>::type;
+//
 
 } // namespace dink
--- src/dink/scope/config/binding_unit_test.cpp
+++ src/dink/scope/config/binding_unit_test.cpp
@@ -9,49 +9,7 @@
 namespace dink {
 namespace {
 
-struct keys_test_t : Test
-{
-    template <int id>
-    struct unique_key_t
-    {};
-
-    template <typename key_p>
-    struct element_t
-    {
-        using key_t = key_p;
-    };
-};
-
-TEST_F(keys_test_t, zero_elements)
-{
-    using expected_tuple_t = std::tuple<>;
-    using input_tuple_t = std::tuple<>;
-
-    using actual_tuple_t = keys_t<input_tuple_t>;
-
-    static_assert(std::same_as<expected_tuple_t, actual_tuple_t>);
-}
-
-TEST_F(keys_test_t, single_element)
-{
-    using expected_tuple_t = std::tuple<unique_key_t<0>>;
-    using input_tuple_t = std::tuple<element_t<std::tuple_element_t<0, expected_tuple_t>>>;
-    using actual_tuple_t = keys_t<input_tuple_t>;
-
-    static_assert(std::same_as<expected_tuple_t, actual_tuple_t>);
-}
-
-TEST_F(keys_test_t, multiple_elements)
-{
-    using expected_tuple_t = std::tuple<unique_key_t<0>, unique_key_t<1>, unique_key_t<2>>;
-    using input_tuple_t = std::tuple<
-        element_t<std::tuple_element_t<0, expected_tuple_t>>, element_t<std::tuple_element_t<1, expected_tuple_t>>,
-        element_t<std::tuple_element_t<2, expected_tuple_t>>>;
-
-    using actual_tuple_t = keys_t<input_tuple_t>;
-
-    static_assert(std::same_as<expected_tuple_t, actual_tuple_t>);
-}
+//
 
 } // namespace
 } // namespace dink
