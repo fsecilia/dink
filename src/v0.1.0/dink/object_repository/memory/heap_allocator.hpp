38e76d0453e3ec8285b2c3e5517abf2bd2603e9d
Frank Secilia <frank.secilia@gmail.com>
2025-09-14 01:49:47 -0400

    memory: add heap_allocator

---
+++ src/v0.1.0/dink/object_repository/memory/heap_allocator.hpp
@@ -0,0 +1,46 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#pragma once
+
+#include <dink/lib.hpp>
+#include <bit>
+#include <cassert>
+#include <memory>
+
+namespace dink {
+
+//! aligned heap allocator, returns unique_ptrs
+struct heap_allocator_t
+{
+    //! deletes from heap using std::free
+    struct deleter_t
+    {
+        auto operator()(void* allocation) const noexcept { std::free(allocation); }
+    };
+
+    //! unique_ptr with custom, stateless deleter
+    using allocation_t = std::unique_ptr<void, deleter_t>;
+
+    /*!
+        allocates from heap using std::aligned_alloc
+
+        \pre align_val is nonzero power of two
+        \pre size is multiple of align_val
+        \returns unique_ptr with stateless deleter
+        \throws std::bad_alloc if allocation fails
+    */
+    auto allocate(std::size_t size, std::align_val_t align_val) const -> allocation_t
+    {
+        auto const alignment = static_cast<std::size_t>(align_val);
+
+        assert(std::has_single_bit(alignment));
+        assert(size == ((size + alignment - 1) & -alignment));
+
+        return allocation_t{std::aligned_alloc(size, alignment)};
+    }
+};
+
+} // namespace dink
