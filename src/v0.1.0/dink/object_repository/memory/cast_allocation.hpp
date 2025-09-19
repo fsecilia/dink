a1d5a0dc93c5e3094317abfc0c6ed34fd6f9b347
Frank Secilia <frank.secilia@gmail.com>
2025-09-19 10:27:33 -0400

    memory: cast_allocation is a function again
    It certainly was not a cpo, but I can't even justify making this a global function object because substituting it isn't useful.
    It's only failure condition is if the ctor throws, and that is better simulated in a test by using a type that throws from its ctor, rather than substituting the cast impl.

--- src/v0.1.0/dink/object_repository/memory/cast_allocation.hpp
+++ src/v0.1.0/dink/object_repository/memory/cast_allocation.hpp
@@ -11,34 +11,22 @@
 
 namespace dink {
 
-namespace detail {
-
-//! cpo for cast_allocation
-template <typename dst_t, typename dst_deleter_t>
-struct allocation_caster_t
-{
-    template <typename allocation_deleter_t, typename... dst_ctor_args_t>
-    [[nodiscard]] auto operator()(
-        std::unique_ptr<void, allocation_deleter_t>&& allocation, dst_ctor_args_t&&... ctor_args
-    ) const -> std::unique_ptr<dst_t, dst_deleter_t>
-    {
-        if (!allocation) return nullptr;
-
-        // perfect forward ctor args to placement new
-        auto* node = new (allocation.get()) dst_t{std::forward<dst_ctor_args_t>(ctor_args)...};
-
-        // transfer ownership, wrapping the allocation's original deleter
-        auto result = std::unique_ptr<dst_t, dst_deleter_t>{node, dst_deleter_t{std::move(allocation.get_deleter())}};
-        allocation.release();
-
-        return result;
-    }
-};
-
-} // namespace detail
-
 //! constructs a specific type in an untyped allocation, then transfers ownership and the original deleter
-template <typename dst_t, typename dst_deleter_t>
-inline constexpr auto cast_allocation = detail::allocation_caster_t<dst_t, dst_deleter_t>{};
+template <typename dst_t, typename dst_deleter_t, typename allocation_deleter_t, typename... dst_ctor_args_t>
+[[nodiscard]] auto cast_allocation(
+    std::unique_ptr<void, allocation_deleter_t>&& allocation, dst_ctor_args_t&&... ctor_args
+) -> std::unique_ptr<dst_t, dst_deleter_t>
+{
+    if (!allocation) return nullptr;
+
+    // perfect forward ctor args to placement new
+    auto* node = new (allocation.get()) dst_t{std::forward<dst_ctor_args_t>(ctor_args)...};
+
+    // transfer ownership, wrapping the allocation's original deleter
+    auto result = std::unique_ptr<dst_t, dst_deleter_t>{node, dst_deleter_t{std::move(allocation.get_deleter())}};
+    allocation.release();
+
+    return result;
+}
 
 } // namespace dink
