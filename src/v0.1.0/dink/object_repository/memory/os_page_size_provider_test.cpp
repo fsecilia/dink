4940c3482a100d350449c23bf778d28db03c7652
Frank Secilia <frank.secilia@gmail.com>
2025-09-20 03:05:10 -0400

    memory: s/page_size/os_page_size_provider
    The arena allocator was originally going to be called "arena_t" and the pool of arenas "pooled_arena_allocator_t". That name is a bit wordy. Now, individual arenas will be called "page_t" and the collection of pages will be called "paged_allocator_t". That would make the name "page_size_t" here confusing, so this revision clarifies that it is the os page size provider.

--- src/v0.1.0/dink/object_repository/memory/os_page_size_provider_test.cpp
+++ src/v0.1.0/dink/object_repository/memory/os_page_size_provider_test.cpp
@@ -3,16 +3,16 @@
     Copyright (C) 2025 Frank Secilia
 */
 
-#include "page_size.hpp"
+#include "os_page_size_provider.hpp"
 #include <dink/test.hpp>
 
 namespace dink {
 namespace posix {
 namespace {
 
-#if dink_page_size_impl == dink_page_size_impl_posix
+#if dink_page_size_provider_impl == dink_page_size_provider_impl_posix
 
-struct posix_page_size_test_t : Test
+struct posix_page_size_provider_test_t : Test
 {
     struct mock_api_t
     {
@@ -28,24 +28,24 @@ struct posix_page_size_test_t : Test
     };
     api_t api{&mock_api};
 
-    using sut_t = page_size_t<api_t>;
+    using sut_t = page_size_provider_t<api_t>;
     sut_t sut{api};
 };
 
-TEST_F(posix_page_size_test_t, succeed)
+TEST_F(posix_page_size_provider_test_t, succeed)
 {
-    auto const expected = fallback_page_size * 2;
-    EXPECT_CALL(mock_api, sysconf(sut_t::sysconf_page_size_name)).WillOnce(Return(static_cast<int>(expected)));
+    auto const expected = fallback_page_size_provider * 2;
+    EXPECT_CALL(mock_api, sysconf(sut_t::sysconf_page_size_provider_name)).WillOnce(Return(static_cast<int>(expected)));
 
     auto const actual = sut();
     ASSERT_EQ(expected, actual);
 }
 
-TEST_F(posix_page_size_test_t, failed_sysconf_uses_fallback)
+TEST_F(posix_page_size_provider_test_t, failed_sysconf_uses_fallback)
 {
-    EXPECT_CALL(mock_api, sysconf(sut_t::sysconf_page_size_name)).WillOnce(Return(static_cast<int>(0)));
+    EXPECT_CALL(mock_api, sysconf(sut_t::sysconf_page_size_provider_name)).WillOnce(Return(static_cast<int>(0)));
 
-    auto const expected = fallback_page_size;
+    auto const expected = fallback_page_size_provider;
     auto const actual = sut();
     ASSERT_EQ(expected, actual);
 }
