b62913b212f8d8395eaae64b759176aa868151da
Frank Secilia <frank.secilia@gmail.com>
2025-09-21 19:50:55 -0400

    alloc: add scoped node factory

--- src/v0.1.0/dink/object_repository/memory/allocators/scoped.hpp
+++ src/v0.1.0/dink/object_repository/memory/allocators/scoped.hpp
@@ -6,9 +6,55 @@
 #pragma once
 
 #include <dink/lib.hpp>
+#include <dink/object_repository/memory/alignment.hpp>
+#include <dink/object_repository/memory/allocators/allocators.hpp>
+#include <dink/object_repository/memory/construct_in_allocation.hpp>
+#include <cassert>
+#include <cstddef>
+#include <memory>
+#include <utility>
 
-namespace dink {
+namespace dink::scoped {
 
-//
+struct node_t
+{
+    node_t* next;
+    void* allocation;
+};
 
-} // namespace dink
+//! creates scoped nodes in an allocation large enough to hold the node out of band before the requested allocation
+template <typename policy_t>
+class node_factory_t
+{
+public:
+    using node_allocator_t = policy_t::node_allocator_t;
+    using node_deleter_t = policy_t::node_deleter_t;
+    using node_t = policy_t::node_t;
+
+    using allocated_node_t = std::unique_ptr<node_t, node_deleter_t>;
+
+    auto operator()(std::size_t size, std::align_val_t align_val) -> allocated_node_t
+    {
+        // request oversized allocation
+        auto const alignment = static_cast<std::size_t>(align_val);
+        auto allocation = node_allocator_.allocate(size + sizeof(node_t) + alignment - 1);
+
+        // lay out node and then manually aligned request
+        auto const node_address = reinterpret_cast<std::byte*>(std::to_address(allocation));
+        auto const aligned_allocation = reinterpret_cast<void*>(
+            (reinterpret_cast<uintptr_t>(node_address) + sizeof(node_t) + alignment - 1) & -alignment
+        );
+
+        // construct node at beginning of allocation
+        return construct_in_allocation<node_t, node_deleter_t>(
+            std::move(allocation), node_t{nullptr, aligned_allocation}
+        );
+    }
+
+    explicit node_factory_t(node_allocator_t node_allocator) noexcept : node_allocator_{std::move(node_allocator)} {}
+
+private:
+    node_allocator_t node_allocator_;
+};
+
+} // namespace dink::scoped
--- src/v0.1.0/dink/object_repository/memory/allocators/scoped_test.cpp
+++ src/v0.1.0/dink/object_repository/memory/allocators/scoped_test.cpp
@@ -6,10 +6,112 @@
 #include "scoped.hpp"
 #include <dink/test.hpp>
 
-namespace dink {
+namespace dink::scoped {
 namespace {
 
-//
+struct scoped_node_factory_test_t : Test
+{
+    struct allocation_deleter_t
+    {
+        auto operator()(void*) noexcept -> void {}
+
+        static auto const default_id = int_t{3};
+        int_t id = default_id;
+    };
+    using allocation_t = std::unique_ptr<void, allocation_deleter_t>;
+
+    struct mock_node_allocator_t
+    {
+        MOCK_METHOD(allocation_t, allocate, (std::size_t size));
+        virtual ~mock_node_allocator_t() = default;
+    };
+    StrictMock<mock_node_allocator_t> mock_node_allocator;
+
+    struct node_allocator_t
+    {
+        auto allocate(std::size_t size) -> allocation_t { return mock_node_allocator->allocate(size); }
+
+        mock_node_allocator_t* mock_node_allocator = nullptr;
+    };
+
+    struct node_ctor_x
+    {};
+
+    struct node_t
+    {
+        node_t(node_t* next, void* allocation) : next{next}, allocation{allocation}
+        {
+            if (throw_exception) throw node_ctor_x{};
+        }
+
+        static inline auto throw_exception = false;
+
+        node_t* next;
+        void* allocation;
+    };
+
+    struct node_deleter_t
+    {
+        allocation_deleter_t allocation_deleter;
+
+        auto operator()(node_t* node) noexcept -> void { node->~node_t(); }
+
+        node_deleter_t(allocation_deleter_t&& allocation_deleter) : allocation_deleter{std::move(allocation_deleter)} {}
+        node_deleter_t() = default;
+    };
+
+    static inline constexpr auto const requested_allocation_size = std::size_t{2048};
+    static inline constexpr auto const requested_allocation_align_val = std::align_val_t{512};
+    static inline constexpr auto const allocation_size
+        = requested_allocation_size + sizeof(node_t) + static_cast<std::size_t>(requested_allocation_align_val) - 1;
+    std::array<std::byte, allocation_size> allocation{};
+
+    struct policy_t
+    {
+        using node_allocator_t = node_allocator_t;
+        using node_deleter_t = node_deleter_t;
+        using node_t = node_t;
+    };
+
+    using sut_t = node_factory_t<policy_t>;
+    sut_t sut{node_allocator_t{&mock_node_allocator}};
+};
+
+TEST_F(scoped_node_factory_test_t, construction_succeeds)
+{
+    auto const expected_allocation_deleter_id = allocation_deleter_t::default_id + 1;
+    auto const expected_allocation_value = std::data(allocation);
+    EXPECT_CALL(mock_node_allocator, allocate(allocation_size))
+        .WillOnce(Return(
+            ByMove(allocation_t{expected_allocation_value, allocation_deleter_t{expected_allocation_deleter_id}})
+        ));
+
+    auto const allocated_node = sut(requested_allocation_size, requested_allocation_align_val);
+
+    ASSERT_EQ(expected_allocation_value, static_cast<void*>(allocated_node.get()));
+    ASSERT_EQ(expected_allocation_deleter_id, allocated_node.get_deleter().allocation_deleter.id);
+    ASSERT_EQ(nullptr, allocated_node->next);
+
+    auto const expected_allocation_begin
+        = align(expected_allocation_value + sizeof(node_t), requested_allocation_align_val);
+    ASSERT_EQ(expected_allocation_begin, allocated_node->allocation);
+}
+
+TEST_F(scoped_node_factory_test_t, construction_fails_when_allocation_throws)
+{
+    EXPECT_CALL(mock_node_allocator, allocate(allocation_size)).WillOnce(Throw(std::bad_alloc{}));
+    EXPECT_THROW((void)sut(requested_allocation_size, requested_allocation_align_val), std::bad_alloc);
+}
+
+TEST_F(scoped_node_factory_test_t, construction_fails_when_ctor_throws)
+{
+    EXPECT_CALL(mock_node_allocator, allocate(allocation_size))
+        .WillOnce(Return(ByMove(allocation_t{std::data(allocation), allocation_deleter_t{}})));
+
+    node_t::throw_exception = true;
+    EXPECT_THROW((void)sut(requested_allocation_size, requested_allocation_align_val), node_ctor_x);
+    node_t::throw_exception = false;
+}
 
 } // namespace
-} // namespace dink
+} // namespace dink::scoped
