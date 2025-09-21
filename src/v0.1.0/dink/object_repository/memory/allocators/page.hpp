fc4d47bd89eecfe519a7ac8faf6df5831ac2fd92
Frank Secilia <frank.secilia@gmail.com>
2025-09-21 00:21:43 -0400

    alloc: s/pending_allocation/reservation
    This more accurately represents what this type is, and as the result of calling reserve().

--- src/v0.1.0/dink/object_repository/memory/allocators/page.hpp
+++ src/v0.1.0/dink/object_repository/memory/allocators/page.hpp
@@ -14,16 +14,16 @@ namespace dink::page {
 /*!
     command to commit allocation from a page after reserving it
 
-    This type is the \ref pending_allocation for the page allocator.
+    This type is the \ref reservation for the page allocator.
 */
 template <typename allocator_t>
-class pending_allocation_t
+class reservation_t
 {
 public:
     auto allocation() const noexcept -> void* { return allocation_begin_; }
     auto commit() noexcept -> void { allocator_->commit(allocation_end_); }
 
-    pending_allocation_t(allocator_t& allocator, void* allocation_begin, void* allocation_end) noexcept
+    reservation_t(allocator_t& allocator, void* allocation_begin, void* allocation_end) noexcept
         : allocator_{&allocator}, allocation_begin_{std::move(allocation_begin)}, allocation_end_{allocation_end}
     {}
 
@@ -36,19 +36,19 @@ private:
 /*!
     allocates from within a region of memory
 
-    This type uses a \ref pending_allocation.
+    This type uses a \ref reservation.
 */
-template <template <typename> class pending_allocation_p>
+template <template <typename> class reservation_p>
 class allocator_t
 {
 public:
-    using pending_allocation_t = pending_allocation_p<allocator_t>;
+    using reservation_t = reservation_p<allocator_t>;
 
     //! maximum allocation size to balance amortization against internal fragmentation
     auto max_allocation_size() const noexcept -> std::size_t { return max_allocation_size_; }
 
     //! \pre align_val is nonzero power of two
-    [[nodiscard]] auto reserve(std::size_t size, std::align_val_t align_val) noexcept -> pending_allocation_t
+    [[nodiscard]] auto reserve(std::size_t size, std::align_val_t align_val) noexcept -> reservation_t
     {
         assert(is_valid_alignment(align_val));
 
@@ -58,7 +58,7 @@ public:
         // make sure worst-case alignment is smaller than limit
         auto const alignment = static_cast<size_t>(align_val);
         auto const total_size_exceeds_limit = size + alignment - 1 > max_allocation_size_;
-        if (total_size_exceeds_limit) return pending_allocation_t{*this, nullptr, nullptr};
+        if (total_size_exceeds_limit) return reservation_t{*this, nullptr, nullptr};
 
         // find next aligned location
         auto const allocation_begin = align(cur_, align_val);
@@ -66,10 +66,10 @@ public:
         // make sure the aligned allocation fits in remaining space
         auto const size_remaining = static_cast<std::size_t>(end_ - allocation_begin);
         auto const end_exceeds_buffer = size_remaining < size;
-        if (end_exceeds_buffer) return pending_allocation_t{*this, nullptr, nullptr};
+        if (end_exceeds_buffer) return reservation_t{*this, nullptr, nullptr};
 
         auto const allocation_end = allocation_begin + size;
-        return pending_allocation_t{*this, allocation_begin, allocation_end};
+        return reservation_t{*this, allocation_begin, allocation_end};
     }
 
     auto commit(void* allocation_end) noexcept -> void { cur_ = static_cast<std::byte*>(allocation_end); }
--- src/v0.1.0/dink/object_repository/memory/allocators/page_test.cpp
+++ src/v0.1.0/dink/object_repository/memory/allocators/page_test.cpp
@@ -9,7 +9,7 @@
 namespace dink::page {
 namespace {
 
-struct page_pending_allocation_test_t : Test
+struct page_reservation_test_t : Test
 {
     struct mock_allocator_t
     {
@@ -28,16 +28,16 @@ struct page_pending_allocation_test_t : Test
     void* const allocation_begin = this;
     void* const allocation_end = this + 1;
 
-    using sut_t = pending_allocation_t<allocator_t>;
+    using sut_t = reservation_t<allocator_t>;
     sut_t sut{allocator, allocation_begin, allocation_end};
 };
 
-TEST_F(page_pending_allocation_test_t, allocation)
+TEST_F(page_reservation_test_t, allocation)
 {
     ASSERT_EQ(allocation_begin, sut.allocation());
 }
 
-TEST_F(page_pending_allocation_test_t, commit)
+TEST_F(page_reservation_test_t, commit)
 {
     EXPECT_CALL(mock_allocator, commit(allocation_end));
 
@@ -49,13 +49,13 @@ TEST_F(page_pending_allocation_test_t, commit)
 struct page_allocator_test_t : Test
 {
     template <typename allocator_t>
-    struct pending_allocation_t
+    struct reservation_t
     {
         allocator_t* allocator;
         void* allocation_begin;
         void* allocation_end;
 
-        pending_allocation_t(allocator_t& allocator, void* allocation_begin, void* allocation_end) noexcept
+        reservation_t(allocator_t& allocator, void* allocation_begin, void* allocation_end) noexcept
             : allocator{&allocator}, allocation_begin{allocation_begin}, allocation_end{allocation_end}
         {}
     };
@@ -68,7 +68,7 @@ struct page_allocator_test_t : Test
     std::align_val_t const align_val = static_cast<std::align_val_t>(alignment);
 
     alignas(alignment) std::array<std::byte, region_size> region;
-    using sut_t = allocator_t<pending_allocation_t>;
+    using sut_t = allocator_t<reservation_t>;
     sut_t sut{std::data(region), region_size, max_allocation_size};
 };
 
@@ -88,7 +88,7 @@ TEST_F(page_allocator_test_t, commit_sets_cur)
     ASSERT_EQ(expected_cur, actual_cur);
 }
 
-TEST_F(page_allocator_test_t, reserve_sets_page_field_in_pending_allocation)
+TEST_F(page_allocator_test_t, reserve_sets_page_field_in_reservation)
 {
     auto const result = sut.reserve(size, align_val);
     ASSERT_EQ(&sut, result.allocator);
@@ -130,23 +130,23 @@ TEST_F(page_allocator_test_t, reserve_succeeds_when_worst_case_is_exactly_max_al
     sut.commit(std::data(region) + 1);
     auto const expected_begin = std::data(region) + alignment;
 
-    auto pending_allocation = sut.reserve(exact_size, align_val);
+    auto reservation = sut.reserve(exact_size, align_val);
 
-    ASSERT_EQ(expected_begin, pending_allocation.allocation_begin);
+    ASSERT_EQ(expected_begin, reservation.allocation_begin);
 }
 
 TEST_F(page_allocator_test_t, reserve_returns_nullptr_when_size_exceeds_max_allocation_size)
 {
     // request exceeds limit, but would fit otherwise
-    auto pending_allocation = sut.reserve(max_allocation_size + 1, std::align_val_t{1});
+    auto reservation = sut.reserve(max_allocation_size + 1, std::align_val_t{1});
 
-    ASSERT_EQ(nullptr, pending_allocation.allocation_begin);
+    ASSERT_EQ(nullptr, reservation.allocation_begin);
 }
 
 TEST_F(page_allocator_test_t, reserve_returns_nonempty_allocation_when_size_is_zero)
 {
-    auto pending_allocation = sut.reserve(0, align_val);
-    ASSERT_LT(pending_allocation.allocation_begin, pending_allocation.allocation_end);
+    auto reservation = sut.reserve(0, align_val);
+    ASSERT_LT(reservation.allocation_begin, reservation.allocation_end);
 }
 
 TEST_F(page_allocator_test_t, reserve_succeeds_when_size_exactly_fits_region)
@@ -156,18 +156,18 @@ TEST_F(page_allocator_test_t, reserve_succeeds_when_size_exactly_fits_region)
     sut.commit(expected_allocation_begin);
 
     // try to reserve allocation that just fits
-    auto pending_allocation = sut.reserve(size, std::align_val_t{1});
+    auto reservation = sut.reserve(size, std::align_val_t{1});
 
-    ASSERT_EQ(expected_allocation_begin, pending_allocation.allocation_begin);
-    ASSERT_EQ(expected_allocation_begin + size, pending_allocation.allocation_end);
+    ASSERT_EQ(expected_allocation_begin, reservation.allocation_begin);
+    ASSERT_EQ(expected_allocation_begin + size, reservation.allocation_end);
 }
 
 TEST_F(page_allocator_test_t, reserve_returns_nullptr_when_worst_case_alignment_forces_size_past_max_allocation_size)
 {
     // allocation size is small enough, but total requested size request exceeds limit
-    auto pending_allocation = sut.reserve(max_allocation_size, align_val);
+    auto reservation = sut.reserve(max_allocation_size, align_val);
 
-    ASSERT_EQ(nullptr, pending_allocation.allocation_begin);
+    ASSERT_EQ(nullptr, reservation.allocation_begin);
 }
 
 TEST_F(page_allocator_test_t, reserve_returns_nullptr_when_size_doesnt_fit_at_end_of_region)
@@ -176,10 +176,10 @@ TEST_F(page_allocator_test_t, reserve_returns_nullptr_when_size_doesnt_fit_at_en
     sut.commit(std::data(region) + region_size - (size - 1));
 
     // try to reserve allocation that extends past end of region
-    auto pending_allocation = sut.reserve(size, std::align_val_t{1});
+    auto reservation = sut.reserve(size, std::align_val_t{1});
 
-    ASSERT_EQ(nullptr, pending_allocation.allocation_begin);
-    ASSERT_EQ(nullptr, pending_allocation.allocation_end);
+    ASSERT_EQ(nullptr, reservation.allocation_begin);
+    ASSERT_EQ(nullptr, reservation.allocation_end);
 }
 
 TEST_F(page_allocator_test_t, reserve_returns_nullptr_when_alignment_doesnt_fit_at_end_of_region)
@@ -188,10 +188,10 @@ TEST_F(page_allocator_test_t, reserve_returns_nullptr_when_alignment_doesnt_fit_
     sut.commit(std::data(region) + region_size - (alignment - 1));
 
     // try to reserve allocation that aligns to end of region
-    auto pending_allocation = sut.reserve(1, align_val);
+    auto reservation = sut.reserve(1, align_val);
 
-    ASSERT_EQ(nullptr, pending_allocation.allocation_begin);
-    ASSERT_EQ(nullptr, pending_allocation.allocation_end);
+    ASSERT_EQ(nullptr, reservation.allocation_begin);
+    ASSERT_EQ(nullptr, reservation.allocation_end);
 }
 
 } // namespace
