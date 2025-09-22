5a0af65ec6f0da9b79877d866aed579939de0cda
Frank Secilia <frank.secilia@gmail.com>
2025-09-22 05:50:35 -0400

    alloc: add thresholding allocator

--- src/v0.1.0/dink/object_repository/memory/allocators/thresholding.hpp
+++ src/v0.1.0/dink/object_repository/memory/allocators/thresholding.hpp
@@ -6,6 +6,8 @@
 #pragma once
 
 #include <dink/lib.hpp>
+#include <cassert>
+#include <cstddef>
 #include <utility>
 #include <variant>
 
@@ -39,6 +41,40 @@ public:
 
 private:
     std::variant<small_object_reservation_t, large_object_reservation_t> strat_;
+};
+
+//! dispatches to small or large object allocator based on requested allocation size
+template <typename policy_t>
+class allocator_t
+{
+public:
+    using small_object_allocator_t = policy_t::small_object_policy_t::allocator_t;
+    using large_object_allocator_t = policy_t::large_object_policy_t::allocator_t;
+    using reservation_t = policy_t::reservation_t;
+
+    auto threshold() const noexcept -> std::size_t { return small_object_allocator_.max_allocation_size(); }
+
+    auto reserve(std::size_t size, std::align_val_t align_val) -> reservation_t
+    {
+        auto const total_allocation_size = size + static_cast<std::size_t>(align_val) - 1;
+        if (total_allocation_size <= small_object_allocator_.max_allocation_size())
+        {
+            return reservation_t{small_object_allocator_.reserve(size, align_val)};
+        }
+
+        return reservation_t{large_object_allocator_.reserve(size, align_val)};
+    }
+
+    allocator_t(
+        small_object_allocator_t small_object_allocator, large_object_allocator_t large_object_allocator
+    ) noexcept
+        : small_object_allocator_{std::move(small_object_allocator)},
+          large_object_allocator_{std::move(large_object_allocator)}
+    {}
+
+private:
+    small_object_allocator_t small_object_allocator_;
+    large_object_allocator_t large_object_allocator_;
 };
 
 } // namespace dink::thresholding
--- src/v0.1.0/dink/object_repository/memory/allocators/thresholding_test.cpp
+++ src/v0.1.0/dink/object_repository/memory/allocators/thresholding_test.cpp
@@ -90,5 +90,117 @@ TEST_F(thresholding_allocator_reservation_test_large_object_allocator_t, commit_
     sut.commit();
 }
 
+// ---------------------------------------------------------------------------------------------------------------------
+
+struct thresholding_allocator_test_t : Test
+{
+    struct reservation_t
+    {
+        enum id_t
+        {
+            default_initialized,
+            small_object,
+            large_object
+        };
+        id_t id = id_t::default_initialized;
+    };
+
+    struct mock_small_object_allocator_t
+    {
+        MOCK_METHOD(std::size_t, max_allocation_size, (), (const, noexcept));
+        MOCK_METHOD(reservation_t::id_t, reserve, (std::size_t size, std::align_val_t align_val));
+        virtual ~mock_small_object_allocator_t() = default;
+    };
+    StrictMock<mock_small_object_allocator_t> mock_small_object_allocator;
+
+    struct small_object_allocator_t
+    {
+        auto max_allocation_size() const noexcept -> std::size_t { return mock->max_allocation_size(); }
+        auto reserve(std::size_t size, std::align_val_t align_val) -> reservation_t::id_t
+        {
+            return mock->reserve(size, align_val);
+        }
+        mock_small_object_allocator_t* mock = nullptr;
+    };
+
+    struct mock_large_object_allocator_t
+    {
+        MOCK_METHOD(reservation_t::id_t, reserve, (std::size_t size, std::align_val_t align_val));
+        virtual ~mock_large_object_allocator_t() = default;
+    };
+    StrictMock<mock_large_object_allocator_t> mock_large_object_allocator;
+
+    struct large_object_allocator_t
+    {
+        auto reserve(std::size_t size, std::align_val_t align_val) -> reservation_t::id_t
+        {
+            return mock->reserve(size, align_val);
+        }
+        mock_large_object_allocator_t* mock = nullptr;
+    };
+
+    struct policy_t
+    {
+        struct small_object_policy_t
+        {
+            using allocator_t = small_object_allocator_t;
+        };
+
+        struct large_object_policy_t
+        {
+            using allocator_t = large_object_allocator_t;
+        };
+
+        using reservation_t = reservation_t;
+    };
+
+    using sut_t = allocator_t<policy_t>;
+    sut_t sut{
+        small_object_allocator_t{&mock_small_object_allocator}, large_object_allocator_t{&mock_large_object_allocator}
+    };
+
+    std::size_t const small_object_allocator_max_allocation_size = 1024;
+    std::align_val_t const expected_align_val = std::align_val_t{16};
+    std::size_t const aligned_threshold_size
+        = small_object_allocator_max_allocation_size - (static_cast<std::size_t>(expected_align_val) - 1);
+};
+
+TEST_F(thresholding_allocator_test_t, threshold_forwards_to_small_object_allocator)
+{
+    EXPECT_CALL(mock_small_object_allocator, max_allocation_size())
+        .WillOnce(Return(small_object_allocator_max_allocation_size));
+    ASSERT_EQ(small_object_allocator_max_allocation_size, sut.threshold());
+}
+
+TEST_F(thresholding_allocator_test_t, reserve_small_allocation_forwards_to_small_object_allocator)
+{
+    EXPECT_CALL(mock_small_object_allocator, max_allocation_size())
+        .WillOnce(Return(small_object_allocator_max_allocation_size));
+
+    auto const expected_allocation_size = aligned_threshold_size;
+    auto const expected_reservation_id = reservation_t::id_t::small_object;
+    EXPECT_CALL(mock_small_object_allocator, reserve(expected_allocation_size, expected_align_val))
+        .WillOnce(Return(expected_reservation_id));
+
+    auto const acutal_reservation = sut.reserve(expected_allocation_size, expected_align_val);
+
+    ASSERT_EQ(expected_reservation_id, acutal_reservation.id);
+}
+
+TEST_F(thresholding_allocator_test_t, reserve_large_allocation_forwards_to_large_object_allocator)
+{
+    EXPECT_CALL(mock_small_object_allocator, max_allocation_size())
+        .WillOnce(Return(small_object_allocator_max_allocation_size));
+
+    auto const expected_allocation_size = aligned_threshold_size + 1;
+    auto const expected_reservation_id = reservation_t::id_t::large_object;
+    EXPECT_CALL(mock_large_object_allocator, reserve(expected_allocation_size, expected_align_val))
+        .WillOnce(Return(expected_reservation_id));
+
+    auto const acutal_reservation = sut.reserve(expected_allocation_size, expected_align_val);
+
+    ASSERT_EQ(expected_reservation_id, acutal_reservation.id);
+}
+
 } // namespace
 } // namespace dink::thresholding
