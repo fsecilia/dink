a1c3c0e8649f006a8be29f2d135a4ced4501a1bc
Frank Secilia <frank.secilia@gmail.com>
2025-09-24 14:16:25 -0400

    tuple: add contains_v

--- src/dink/tuple.hpp
+++ src/dink/tuple.hpp
@@ -6,6 +6,8 @@
 #pragma once
 
 #include <dink/lib.hpp>
+#include <tuple>
+#include <type_traits>
 
 namespace dink::tuple {
 
@@ -16,5 +18,18 @@ template <typename>
 inline constexpr auto const dependent_false_v = false;
 
 } // namespace detail
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+template <typename tuple_t, typename element_t>
+struct contains_f;
+
+template <typename element_t, typename... elements_t>
+struct contains_f<std::tuple<elements_t...>, element_t> : std::disjunction<std::is_same<element_t, elements_t>...>
+{};
+
+//! true if tuple_t has an element of type element_t
+template <typename tuple_t, typename element_t>
+inline constexpr auto const contains_v = contains_f<tuple_t, element_t>::value;
 
 } // namespace dink::tuple
--- src/dink/tuple_test.cpp
+++ src/dink/tuple_test.cpp
@@ -9,7 +9,72 @@
 namespace dink::tuple {
 namespace {
 
-//
+// shorthand for tests
+template <typename... elements_t>
+using t = std::tuple<elements_t...>;
+
+template <int id>
+struct unique_value_t
+{};
+
+using v0 = unique_value_t<0>;
+using v1 = unique_value_t<1>;
+using v2 = unique_value_t<2>;
+
+// clang-format off
+
+// exhaustive test of all combinations of 0, 1, 2 of lengths up to 3
+
+static_assert(!contains_v<t<>, v0>);
+static_assert(!contains_v<t<>, v1>);
+static_assert(!contains_v<t<>, v2>);
+static_assert(contains_v<t<v0>, v0>);
+static_assert(!contains_v<t<v0>, v1>);
+static_assert(!contains_v<t<v0>, v2>);
+static_assert(!contains_v<t<v1>, v0>);
+static_assert(contains_v<t<v1>, v1>);
+static_assert(!contains_v<t<v1>, v2>);
+static_assert(!contains_v<t<v2>, v0>);
+static_assert(!contains_v<t<v2>, v1>);
+static_assert(contains_v<t<v2>, v2>);
+static_assert(contains_v<t<v0, v1>, v0>);
+static_assert(contains_v<t<v0, v1>, v1>);
+static_assert(!contains_v<t<v0, v1>, v2>);
+static_assert(contains_v<t<v0, v2>, v0>);
+static_assert(!contains_v<t<v0, v2>, v1>);
+static_assert(contains_v<t<v0, v2>, v2>);
+static_assert(contains_v<t<v1, v0>, v0>);
+static_assert(contains_v<t<v1, v0>, v1>);
+static_assert(!contains_v<t<v1, v0>, v2>);
+static_assert(!contains_v<t<v1, v2>, v0>);
+static_assert(contains_v<t<v1, v2>, v1>);
+static_assert(contains_v<t<v1, v2>, v2>);
+static_assert(contains_v<t<v2, v0>, v0>);
+static_assert(!contains_v<t<v2, v0>, v1>);
+static_assert(contains_v<t<v2, v0>, v2>);
+static_assert(!contains_v<t<v2, v1>, v0>);
+static_assert(contains_v<t<v2, v1>, v1>);
+static_assert(contains_v<t<v2, v1>, v2>);
+static_assert(contains_v<t<v0, v1, v2>, v0>);
+static_assert(contains_v<t<v0, v1, v2>, v1>);
+static_assert(contains_v<t<v0, v1, v2>, v2>);
+static_assert(contains_v<t<v0, v2, v1>, v0>);
+static_assert(contains_v<t<v0, v2, v1>, v1>);
+static_assert(contains_v<t<v0, v2, v1>, v2>);
+static_assert(contains_v<t<v1, v0, v2>, v0>);
+static_assert(contains_v<t<v1, v0, v2>, v1>);
+static_assert(contains_v<t<v1, v0, v2>, v2>);
+static_assert(contains_v<t<v1, v2, v0>, v0>);
+static_assert(contains_v<t<v1, v2, v0>, v1>);
+static_assert(contains_v<t<v1, v2, v0>, v2>);
+static_assert(contains_v<t<v2, v0, v1>, v0>);
+static_assert(contains_v<t<v2, v0, v1>, v1>);
+static_assert(contains_v<t<v2, v0, v1>, v2>);
+static_assert(contains_v<t<v2, v1, v0>, v0>);
+static_assert(contains_v<t<v2, v1, v0>, v1>);
+static_assert(contains_v<t<v2, v1, v0>, v2>);
+
+// clang-format on
 
 } // namespace
 } // namespace dink::tuple
