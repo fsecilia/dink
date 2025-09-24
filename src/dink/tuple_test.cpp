a56589ed75cd36171e03e3839b78e82cd6aa02f1
Frank Secilia <frank.secilia@gmail.com>
2025-09-24 13:56:32 -0400

    tuple: shell module

---
+++ src/dink/tuple_test.cpp
@@ -0,0 +1,15 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#include <dink/test.hpp>
+#include <dink/tuple.hpp>
+
+namespace dink::tuple {
+namespace {
+
+//
+
+} // namespace
+} // namespace dink::tuple
