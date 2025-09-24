09b7ad187075d6968b937af7f229f459a6d74d2a
Frank Secilia <frank.secilia@gmail.com>
2025-09-24 13:57:07 -0400

    tuple: add dependent_false_v

--- src/dink/tuple.hpp
+++ src/dink/tuple.hpp
@@ -9,6 +9,12 @@
 
 namespace dink::tuple {
 
-//
+namespace detail {
+
+// constexpr false, but dependent on a template parameter to delay evaluation
+template <typename>
+inline constexpr auto const dependent_false_v = false;
+
+} // namespace detail
 
 } // namespace dink::tuple
