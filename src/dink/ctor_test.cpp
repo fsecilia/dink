28b719a5462a1a9f2dbe2b853ca2138bb1e455b0
Frank Secilia <frank.secilia@gmail.com>
2025-09-25 23:12:28 -0400

    ctor: settle on arity implementation

--- src/dink/ctor_test.cpp
+++ src/dink/ctor_test.cpp
@@ -6,10 +6,7 @@
 #include "ctor.hpp"
 #include <dink/test.hpp>
 #include <dink/meta.hpp>
-#include <dink/tuple.hpp>
-#include <tuple>
 #include <type_traits>
-#include <utility>
 
 namespace dink {
 // namespace {
@@ -30,208 +27,13 @@ struct single_arg_t
     operator deduced_t();
 };
 
-#if 0
-
-// this version uses parspec and type packs, but requires an entry point to kick it all off
-
-template <typename constructed_t, bool is_constructible, typename... args_t>
-struct ctor_arity_f;
-
-template <typename constructed_t, typename... args_t>
-struct ctor_arity_f<constructed_t, true, args_t...>
-{
-    static constexpr std::size_t const value = sizeof...(args_t);
-};
-
-template <typename constructed_t, typename next_arg_t, typename... remaining_args_t>
-struct ctor_arity_f<constructed_t, false, next_arg_t, remaining_args_t...>
-{
-    static constexpr std::size_t const value = ctor_arity_f<
-        constructed_t, std::is_constructible_v<constructed_t, remaining_args_t...>, remaining_args_t...>::value;
-};
-
-template <typename constructed_t, typename next_arg_t, typename remaining_arg_t>
-struct ctor_arity_f<constructed_t, false, next_arg_t, remaining_arg_t>
-{
-    static constexpr std::size_t const value = ctor_arity_f<
-        constructed_t, std::is_constructible_v<constructed_t, single_arg_t<constructed_t>>,
-        single_arg_t<constructed_t>>::value;
-};
-
-template <typename constructed_t>
-struct ctor_arity_f<constructed_t, false>
-{
-    static_assert(std::is_default_constructible_v<constructed_t>, "could not deduce constructor arity");
-    static constexpr std::size_t const value = 0;
-};
-
-template <typename constructed_t, typename index_sequence_t>
-struct ctor_arity_dispatcher_f;
-
-template <typename constructed_t, std::size_t... indices>
-struct ctor_arity_dispatcher_f<constructed_t, std::index_sequence<indices...>>
-    : ctor_arity_f<
-          constructed_t, std::is_constructible_v<constructed_t, meta::indexed_type_t<arg_t, indices>...>,
-          meta::indexed_type_t<arg_t, indices>...>
-{};
-
-template <typename constructed_t>
-inline constexpr auto const ctor_arity_v
-    = ctor_arity_dispatcher_f<constructed_t, std::make_index_sequence<dink_max_deduced_arity>>::value;
-
-#endif
-
-#if 0
-
-// this is the shortest version, using a triply nested lambda to keep nearby logic nearby and index sequences everywhere
+// sentinel value used to indicate deduction failed
+static inline constexpr auto ctor_not_found = static_cast<std::size_t>(-1);
 
 template <typename constructed_t, typename index_sequence_t>
 struct ctor_arity_f;
 
-template <typename constructed_t, std::size_t index, std::size_t... remaining_indices>
-struct ctor_arity_f<constructed_t, std::index_sequence<index, remaining_indices...>>
-{
-private:
-    using current_sequence_t = std::index_sequence<index, remaining_indices...>;
-
-    static constexpr bool is_constructible_now_v = []() {
-        if constexpr (sizeof...(remaining_indices) + 1 == 1)
-        {
-            return std::is_constructible_v<constructed_t, single_arg_t<constructed_t>>;
-        }
-        else
-        {
-            return []<std::size_t... indices>(std::index_sequence<indices...>) {
-                return std::is_constructible_v<constructed_t, meta::indexed_type_t<arg_t, indices>...>;
-            }(current_sequence_t{});
-        }
-    }();
-
-public:
-    static constexpr std::size_t const value = [] {
-        if constexpr (is_constructible_now_v) { return sizeof...(remaining_indices) + 1; }
-        else { return ctor_arity_f<constructed_t, std::index_sequence<remaining_indices...>>::value; }
-    }();
-};
-
-template <typename constructed_t>
-struct ctor_arity_f<constructed_t, std::index_sequence<>>
-{
-    static constexpr std::size_t const value = std::is_constructible_v<constructed_t> ? 0 : 0;
-};
-
-template <typename constructed_t>
-inline constexpr auto const ctor_arity_v
-    = ctor_arity_f<constructed_t, std::make_index_sequence<dink_max_deduced_arity>>::value;
-
-#endif
-
-#if 0
-
-// this is similar to the lambda version, but uses named functions. it also removes an if constexpr by using an overload
-
-template <typename constructed_t, typename index_sequence_t>
-struct ctor_arity_f;
-
-template <typename constructed_t, std::size_t index, std::size_t... remaining_indices>
-struct ctor_arity_f<constructed_t, std::index_sequence<index, remaining_indices...>>
-{
-private:
-    template <std::size_t... indices>
-    static constexpr auto is_constructible(std::index_sequence<indices...>) noexcept -> bool
-    {
-        return std::is_constructible_v<constructed_t, meta::indexed_type_t<arg_t, indices>...>;
-    }
-
-    static constexpr auto is_constructible(std::index_sequence<index>) noexcept -> bool
-    {
-        return std::is_constructible_v<constructed_t, single_arg_t<constructed_t>>;
-    }
-
-    static constexpr auto value_or_next() noexcept -> std::size_t
-    {
-        if constexpr (is_constructible(std::index_sequence<index, remaining_indices...>{}))
-        {
-            return sizeof...(remaining_indices) + 1;
-        }
-        else { return ctor_arity_f<constructed_t, std::index_sequence<remaining_indices...>>::value; }
-    }
-
-public:
-    static constexpr std::size_t const value = value_or_next();
-};
-
-template <typename constructed_t>
-struct ctor_arity_f<constructed_t, std::index_sequence<>>
-{
-    static_assert(std::is_default_constructible_v<constructed_t>, "could not deduce constructor arity");
-    static constexpr std::size_t const value = 0;
-};
-
-template <typename constructed_t>
-inline constexpr auto const ctor_arity_v
-    = ctor_arity_f<constructed_t, std::make_index_sequence<dink_max_deduced_arity>>::value;
-
-#endif
-
-#if 0
-
-// this verison uses index sequences all the way down, but still uses partial specialization to remove the overload
-
-template <typename constructed_t, typename index_sequence_t>
-struct ctor_arity_f;
-
-template <typename constructed_t, std::size_t index, std::size_t... remaining_indices>
-struct ctor_arity_f<constructed_t, std::index_sequence<index, remaining_indices...>>
-{
-private:
-    static constexpr auto value_or_next() noexcept -> std::size_t
-    {
-        if constexpr (std::is_constructible_v<constructed_t, arg_t, meta::indexed_type_t<arg_t, remaining_indices>...>)
-        {
-            return sizeof...(remaining_indices) + 1;
-        }
-        else { return ctor_arity_f<constructed_t, std::index_sequence<remaining_indices...>>::value; }
-    }
-
-public:
-    static constexpr std::size_t const value = value_or_next();
-};
-
-template <typename constructed_t, std::size_t index>
-struct ctor_arity_f<constructed_t, std::index_sequence<index>>
-{
-private:
-    static constexpr auto value_or_next() noexcept -> std::size_t
-    {
-        if constexpr (std::is_constructible_v<constructed_t, single_arg_t<constructed_t>>) { return 1; }
-        else { return ctor_arity_f<constructed_t, std::index_sequence<>>::value; }
-    }
-
-public:
-    static constexpr std::size_t const value = value_or_next();
-};
-
-template <typename constructed_t>
-struct ctor_arity_f<constructed_t, std::index_sequence<>>
-{
-    static_assert(std::is_default_constructible_v<constructed_t>, "could not deduce constructor arity");
-    static constexpr std::size_t const value = 0;
-};
-
-template <typename constructed_t>
-inline constexpr auto const ctor_arity_v
-    = ctor_arity_f<constructed_t, std::make_index_sequence<dink_max_deduced_arity>>::value;
-
-#endif
-
-#if 1
-
-// this verison uses index sequences all the way down and parspec, but one lambda per to get the if constexpr
-
-template <typename constructed_t, typename index_sequence_t>
-struct ctor_arity_f;
-
+// general case
 template <typename constructed_t, std::size_t index, std::size_t... remaining_indices>
 struct ctor_arity_f<constructed_t, std::index_sequence<index, remaining_indices...>>
 {
@@ -244,6 +46,7 @@ struct ctor_arity_f<constructed_t, std::index_sequence<index, remaining_indices.
     }();
 };
 
+// single arg case
 template <typename constructed_t, std::size_t index>
 struct ctor_arity_f<constructed_t, std::index_sequence<index>>
 {
@@ -253,18 +56,19 @@ struct ctor_arity_f<constructed_t, std::index_sequence<index>>
     }();
 };
 
+// base case
 template <typename constructed_t>
 struct ctor_arity_f<constructed_t, std::index_sequence<>>
 {
-    static_assert(std::is_default_constructible_v<constructed_t>, "could not deduce constructor arity");
-    static constexpr std::size_t const value = 0;
+    static constexpr std::size_t const value = std::is_default_constructible_v<constructed_t> ? 0 : ctor_not_found;
 };
 
 template <typename constructed_t>
-inline constexpr auto const ctor_arity_v
-    = ctor_arity_f<constructed_t, std::make_index_sequence<dink_max_deduced_arity>>::value;
-
-#endif
+inline constexpr auto ctor_arity_v = []() noexcept -> std::size_t {
+    constexpr auto result = ctor_arity_f<constructed_t, std::make_index_sequence<dink_max_deduced_arity>>::value;
+    static_assert(result != ctor_not_found, "could not deduce ctor arity");
+    return result;
+}();
 
 template <typename... args_t>
 struct constructed_by_t
