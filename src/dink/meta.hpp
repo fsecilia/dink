19e8d09bc091160bf68617ad723b2221469e77ec
Frank Secilia <frank.secilia@gmail.com>
2025-09-29 11:07:10 -0400

    meta: add indexed_type_t

--- src/dink/meta.hpp
+++ src/dink/meta.hpp
@@ -15,4 +15,8 @@ namespace dink::meta {
 template <typename>
 inline constexpr auto dependent_false_v = false;
 
+//! consumes an index to produce a type; useful for repeating type_t n times from an index sequence of length n
+template <typename type_t, std::size_t index>
+using indexed_type_t = type_t;
+
 } // namespace dink::meta
