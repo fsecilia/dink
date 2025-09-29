d7d444dbb2def45465021fee17179cd82b2993e8
Frank Secilia <frank.secilia@gmail.com>
2025-09-29 14:42:33 -0400

    type_list: add append_t

--- src/dink/type_list.hpp
+++ src/dink/type_list.hpp
@@ -6,6 +6,7 @@
 #pragma once
 
 #include <dink/lib.hpp>
+#include <type_traits>
 
 namespace dink {
 
@@ -13,4 +14,22 @@ namespace dink {
 template <typename... types_t>
 struct type_list_t;
 
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace type_list {
+
+template <typename type_list_t, typename element_t>
+struct append_f;
+
+template <typename element_t, typename... elements_t>
+struct append_f<type_list_t<elements_t...>, element_t>
+{
+    using type = type_list_t<elements_t..., element_t>;
+};
+
+//! appends element to type list
+template <typename type_list_t, typename element_t>
+using append_t = append_f<type_list_t, element_t>::type;
+
+} // namespace type_list
 } // namespace dink
--- src/dink/type_list_test.cpp
+++ src/dink/type_list_test.cpp
@@ -6,8 +6,29 @@
 #include "type_list.hpp"
 #include <dink/test.hpp>
 
-namespace dink {
+namespace dink::type_list {
 namespace {
 
+// arbitrary, unique types
+struct v0;
+struct v1;
+struct v2;
+struct v3;
+
+// shorthand for type list
+template <typename... elements_t>
+using t = type_list_t<elements_t...>;
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+// append to empty list
+static_assert(std::is_same_v<append_t<t<>, v0>, t<v0>>);
+
+// append to list with one element
+static_assert(std::is_same_v<append_t<t<v0>, v1>, t<v0, v1>>);
+
+// append to list with multiple elements
+static_assert(std::is_same_v<append_t<t<v0, v1, v2>, v3>, t<v0, v1, v2, v3>>);
+
 } // namespace
-} // namespace dink
+} // namespace dink::type_list
