b1b3061a879ca39377cff4634537e7ff48eb952e
Frank Secilia <frank.secilia@gmail.com>
2025-09-29 11:10:31 -0400

    type_list: shell module

---
+++ src/dink/type_list_test.cpp
@@ -0,0 +1,13 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#include "type_list.hpp"
+#include <dink/test.hpp>
+
+namespace dink {
+namespace {
+
+} // namespace
+} // namespace dink
