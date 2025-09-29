df4a337536202dc921a500905aaa4337bfd20d84
Frank Secilia <frank.secilia@gmail.com>
2025-09-29 10:30:27 -0400

    meta: shell module

---
+++ src/dink/meta.hpp
@@ -0,0 +1,16 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+    
+    \brief metaprogramming utilities
+*/
+
+#pragma once
+
+#include <dink/lib.hpp>
+
+namespace dink::meta {
+
+//
+
+} // namespace dink::meta
