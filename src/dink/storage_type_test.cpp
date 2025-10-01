f376cbd0ed132472d55a34f2a970919d8c0b6444
Frank Secilia <frank.secilia@gmail.com>
2025-10-01 06:17:43 -0400

    storage_type: add module

---
+++ src/dink/storage_type_test.cpp
@@ -0,0 +1,15 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#include "storage_type.hpp"
+#include <dink/test.hpp>
+
+namespace dink {
+namespace {
+
+//
+
+} // namespace
+} // namespace dink
