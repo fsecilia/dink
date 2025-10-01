8d9d8cd407a83373f4b222dc40c54b88a9980033
Frank Secilia <frank.secilia@gmail.com>
2025-10-01 04:26:58 -0400

    unqualified: add unqualified_t

--- src/dink/unqualified.hpp
+++ src/dink/unqualified.hpp
@@ -6,9 +6,110 @@
 #pragma once
 
 #include <dink/lib.hpp>
+#include <functional>
+#include <memory>
 
 namespace dink {
 
-//
+namespace detail {
+
+//! metafunction to remove all ref, cv, and pointer qualifiers, including smart pointers and reference wrappers
+template <typename type_t>
+struct unqualified_f;
+
+//! base case: type is unmodified
+template <typename type_t>
+struct unqualified_f
+{
+    using type = type_t;
+};
+
+//! removes lvalue reference
+template <typename type_t>
+struct unqualified_f<type_t&> : unqualified_f<type_t>
+{};
+
+//! removes rvalue reference
+template <typename type_t>
+struct unqualified_f<type_t&&> : unqualified_f<type_t>
+{};
+
+//! removes const
+template <typename type_t>
+struct unqualified_f<type_t const> : unqualified_f<type_t>
+{};
+
+//! removes volatile
+template <typename type_t>
+struct unqualified_f<type_t volatile> : unqualified_f<type_t>
+{};
+
+//! removes pointer
+template <typename type_t>
+struct unqualified_f<type_t*> : unqualified_f<type_t>
+{};
+
+//! pointers to function are unmodified; pointer is not removed
+template <typename return_t, typename... args_t>
+struct unqualified_f<return_t (*)(args_t...)>
+{
+    using type = return_t (*)(args_t...);
+};
+
+//! decays function to function pointer
+template <typename return_t, typename... args_t>
+struct unqualified_f<return_t(args_t...)> : unqualified_f<return_t (*)(args_t...)>
+{};
+
+//! removes unsized array
+template <typename type_t>
+struct unqualified_f<type_t[]> : unqualified_f<type_t>
+{};
+
+//! removes const unsized array; this specialization is necessary to break a tie between const and unsized array
+template <typename type_t>
+struct unqualified_f<type_t const[]> : unqualified_f<type_t>
+{};
+
+//! removes sized array
+template <typename type_t, std::size_t size>
+struct unqualified_f<type_t[size]> : unqualified_f<type_t>
+{};
+
+//! removes const sized array; this specialization is necessary to break a tie between const and sized array
+template <typename type_t, std::size_t size>
+struct unqualified_f<type_t const[size]> : unqualified_f<type_t>
+{};
+
+//! removes reference_wrapper
+template <typename type_t>
+struct unqualified_f<std::reference_wrapper<type_t>> : unqualified_f<type_t>
+{};
+
+//! removes unique_ptr
+template <typename type_t, typename deleter_t>
+struct unqualified_f<std::unique_ptr<type_t, deleter_t>> : unqualified_f<type_t>
+{};
+
+//! removes shared_ptr
+template <typename type_t>
+struct unqualified_f<std::shared_ptr<type_t>> : unqualified_f<type_t>
+{};
+
+//! removes weak_ptr
+template <typename type_t>
+struct unqualified_f<std::weak_ptr<type_t>> : unqualified_f<type_t>
+{};
+
+} // namespace detail
+
+/*!
+    gets core, unqualified type
+
+    This trait removes references, cv-qualifiers, pointers, smart pointers, and reference wrappers to distill a type
+    down to its fundamental form.
+*/
+template <typename type_t>
+using unqualified_t = typename detail::unqualified_f<type_t>::type;
 
 } // namespace dink
--- src/dink/unqualified_test.cpp
+++ src/dink/unqualified_test.cpp
@@ -9,7 +9,90 @@
 namespace dink {
 namespace {
 
-//
+// arbitrary unique type
+struct type_t
+{};
+
+// deleter for unique_ptrs
+struct deleter_t
+{};
+
+// arbitrary size for arrays
+constexpr auto array_size = 10;
+
+// return type for functions
+struct return_t
+{};
+
+// arg type for functions
+struct arg_1_t
+{};
+
+// arg type for functions
+struct arg_2_t
+{};
+
+// basic types
+static_assert(std::is_same_v<unqualified_t<type_t>, type_t>);
+static_assert(std::is_same_v<unqualified_t<type_t&>, type_t>);
+static_assert(std::is_same_v<unqualified_t<type_t&&>, type_t>);
+static_assert(std::is_same_v<unqualified_t<type_t const>, type_t>);
+static_assert(std::is_same_v<unqualified_t<type_t volatile>, type_t>);
+static_assert(std::is_same_v<unqualified_t<type_t*>, type_t>);
+
+// function types
+static_assert(std::is_same_v<unqualified_t<void()>, void (*)()>);
+static_assert(std::is_same_v<unqualified_t<void (*)()>, void (*)()>);
+static_assert(std::is_same_v<unqualified_t<return_t()>, return_t (*)()>);
+static_assert(std::is_same_v<unqualified_t<return_t (*)()>, return_t (*)()>);
+static_assert(std::is_same_v<unqualified_t<void(arg_1_t)>, void (*)(arg_1_t)>);
+static_assert(std::is_same_v<unqualified_t<void (*)(arg_1_t)>, void (*)(arg_1_t)>);
+static_assert(std::is_same_v<unqualified_t<return_t(arg_1_t)>, return_t (*)(arg_1_t)>);
+static_assert(std::is_same_v<unqualified_t<return_t (*)(arg_1_t)>, return_t (*)(arg_1_t)>);
+static_assert(std::is_same_v<unqualified_t<void(arg_1_t, arg_2_t)>, void (*)(arg_1_t, arg_2_t)>);
+static_assert(std::is_same_v<unqualified_t<void (*)(arg_1_t, arg_2_t)>, void (*)(arg_1_t, arg_2_t)>);
+static_assert(std::is_same_v<unqualified_t<return_t(arg_1_t, arg_2_t)>, return_t (*)(arg_1_t, arg_2_t)>);
+static_assert(std::is_same_v<unqualified_t<return_t (*)(arg_1_t, arg_2_t)>, return_t (*)(arg_1_t, arg_2_t)>);
+
+// array types
+static_assert(std::is_same_v<unqualified_t<type_t[]>, type_t>);
+static_assert(std::is_same_v<unqualified_t<type_t const[]>, type_t>);
+static_assert(std::is_same_v<unqualified_t<type_t[array_size]>, type_t>);
+static_assert(std::is_same_v<unqualified_t<type_t const[array_size]>, type_t>);
+
+// composite types
+static_assert(std::is_same_v<unqualified_t<std::reference_wrapper<type_t>>, type_t>);
+static_assert(std::is_same_v<unqualified_t<std::unique_ptr<type_t>>, type_t>);
+static_assert(std::is_same_v<unqualified_t<std::unique_ptr<type_t, deleter_t>>, type_t>);
+static_assert(std::is_same_v<unqualified_t<std::shared_ptr<type_t>>, type_t>);
+static_assert(std::is_same_v<unqualified_t<std::weak_ptr<type_t>>, type_t>);
+
+// type combinations
+static_assert(std::is_same_v<unqualified_t<type_t const&>, type_t>);
+static_assert(std::is_same_v<unqualified_t<type_t volatile*>, type_t>);
+static_assert(std::is_same_v<unqualified_t<type_t const*&>, type_t>);
+static_assert(std::is_same_v<unqualified_t<type_t**>, type_t>);
+static_assert(std::is_same_v<unqualified_t<type_t const* volatile*&&>, type_t>);
+static_assert(std::is_same_v<unqualified_t<return_t (*const)()>, return_t (*)()>);
+static_assert(std::is_same_v<unqualified_t<return_t (*volatile&)(arg_1_t)>, return_t (*)(arg_1_t)>);
+static_assert(std::is_same_v<unqualified_t<return_t (*&&)(arg_1_t, arg_2_t)>, return_t (*)(arg_1_t, arg_2_t)>);
+
+static_assert(std::is_same_v<unqualified_t<std::reference_wrapper<type_t&>>, type_t>);
+static_assert(std::is_same_v<unqualified_t<std::reference_wrapper<type_t const&>>, type_t>);
+static_assert(std::is_same_v<unqualified_t<std::reference_wrapper<type_t> const>, type_t>);
+static_assert(std::is_same_v<unqualified_t<std::reference_wrapper<type_t const>>, type_t>);
+static_assert(std::is_same_v<unqualified_t<std::reference_wrapper<type_t const> const>, type_t>);
+
+static_assert(std::is_same_v<unqualified_t<std::unique_ptr<type_t> const&>, type_t>);
+static_assert(std::is_same_v<unqualified_t<std::shared_ptr<type_t*>>, type_t>);
+static_assert(std::is_same_v<unqualified_t<std::shared_ptr<type_t const[]>>, type_t>);
+static_assert(std::is_same_v<unqualified_t<std::shared_ptr<type_t const[array_size]> const&>, type_t>);
+
+static_assert(std::is_same_v<unqualified_t<std::weak_ptr<type_t const>>, type_t>);
+static_assert(std::is_same_v<unqualified_t<std::weak_ptr<type_t> const>, type_t>);
+static_assert(std::is_same_v<unqualified_t<std::weak_ptr<type_t const> const>, type_t>);
+
+static_assert(std::is_same_v<unqualified_t<std::unique_ptr<std::reference_wrapper<type_t const&>> const&>, type_t>);
 
 } // namespace
 } // namespace dink
