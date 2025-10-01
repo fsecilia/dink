ab8221a5b49688a98f00a2f47c4a76c2932d7824
Frank Secilia <frank.secilia@gmail.com>
2025-09-30 22:25:09 -0400

    meta: add dependent_bool_v

--- src/dink/meta.hpp
+++ src/dink/meta.hpp
@@ -11,9 +11,23 @@
 
 namespace dink::meta {
 
-//! constexpr false, but dependent on a template parameter to delay evaluation
+/*!
+    constexpr false, but dependent on a template parameter to delay evaluation
+
+    This is useful to trigger a static assert when the condition is already known to be false. The static assert
+    message will contain the contents of the passed typename.
+*/
 template <typename>
 inline constexpr auto dependent_false_v = false;
+
+/*!
+    constexpr bool, but dependent on a template parameter to delay evaluation
+
+    This is useful to trigger a static assert conditionally. The static assert message will contain the contents of the
+    passed typename.
+*/
+template <bool condition, typename>
+inline constexpr auto dependent_bool_v = condition;
 
 //! consumes an index to produce a type; useful for repeating type_t n times from an index sequence of length n
 template <typename type_t, std::size_t index>
