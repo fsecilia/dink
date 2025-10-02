28f726b638901f2d99404f1348bc523ca66e35a4
Frank Secilia <frank.secilia@gmail.com>
2025-10-02 09:41:17 -0400

    add factory_invoker_t

---
+++ src/dink/factory_invoker.hpp
@@ -0,0 +1,56 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#pragma once
+
+#include <dink/lib.hpp>
+#include <utility>
+
+namespace dink {
+
+namespace factory_invoker::detail {
+
+//! consumes indices to produce args backed by a resolver
+template <typename arg_t, typename single_arg_t>
+struct indexed_arg_factory_t
+{
+    template <std::size_t arity, std::size_t index>
+    static constexpr auto create(auto& resolver) -> auto // returns different types based on arity
+    {
+        if constexpr (arity == 1) return single_arg_t{arg_t{resolver}};
+        else return arg_t{resolver};
+    }
+};
+
+//! converts an index sequence to args and invokes an instance factory with them
+template <typename constructed_t, typename indexed_arg_factory_t, typename index_sequence_t>
+struct arity_dispatcher_t;
+
+template <typename constructed_t, typename indexed_arg_factory_t, std::size_t... indices>
+struct arity_dispatcher_t<constructed_t, indexed_arg_factory_t, std::index_sequence<indices...>>
+{
+    constexpr auto operator()(auto& instance_factory, auto& resolver) const -> constructed_t
+    {
+        return instance_factory(indexed_arg_factory_t{}.template create<sizeof...(indices), indices>(resolver)...);
+    }
+};
+
+} // namespace factory_invoker::detail
+
+//! invokes a constructed_t factory using arity to determine how many args backed by a resolver to pass, and what type.
+template <typename constructed_t, std::size_t arity, typename arg_t, typename single_arg_t>
+class factory_invoker_t
+{
+public:
+    constexpr auto operator()(auto& factory, auto& resolver) const -> constructed_t
+    {
+        using indexed_arg_factory_t = factory_invoker::detail::indexed_arg_factory_t<arg_t, single_arg_t>;
+        using arity_dispatcher_t = factory_invoker::detail::arity_dispatcher_t<
+            constructed_t, indexed_arg_factory_t, std::make_index_sequence<arity>>;
+        return arity_dispatcher_t{}(factory, resolver);
+    }
+};
+
+} // namespace dink
---
+++ src/dink/factory_invoker_test.cpp
@@ -0,0 +1,188 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#include "factory_invoker.hpp"
+#include <dink/test.hpp>
+
+namespace dink {
+
+namespace factory_invoker::detail {
+namespace {
+
+struct factory_invoker_detail_test_t : Test
+{
+    struct resolver_t
+    {};
+
+    struct arg_t
+    {
+        resolver_t& resolver;
+    };
+
+    struct single_arg_t
+    {
+        arg_t arg;
+    };
+
+    resolver_t resolver{};
+};
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+struct factory_invoker_detail_indexed_arg_factory_test_t : factory_invoker_detail_test_t
+{
+    using sut_t = indexed_arg_factory_t<arg_t, single_arg_t>;
+
+    // when creating a single arg, the type is single_arg_t
+    static_assert(std::is_same_v<single_arg_t, decltype(std::declval<sut_t>().template create<1, 0>(resolver))>);
+
+    // when creating multiple args, the type is arg_t
+    static_assert(std::is_same_v<arg_t, decltype(std::declval<sut_t>().template create<2, 0>(resolver))>);
+    static_assert(std::is_same_v<arg_t, decltype(std::declval<sut_t>().template create<2, 1>(resolver))>);
+};
+
+TEST_F(factory_invoker_detail_indexed_arg_factory_test_t, arg_is_initialized_with_resolver)
+{
+    ASSERT_EQ(&resolver, &(sut_t{}.template create<2, 0>(resolver).resolver));
+    ASSERT_EQ(&resolver, &(sut_t{}.template create<2, 1>(resolver).resolver));
+}
+
+TEST_F(factory_invoker_detail_indexed_arg_factory_test_t, single_arg_is_initialized_with_resolver)
+{
+    ASSERT_EQ(&resolver, &(sut_t{}.template create<1, 0>(resolver).arg.resolver));
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+struct factory_invoker_detail_dispatcher_test_t : factory_invoker_detail_test_t
+{
+    struct constructed_t
+    {
+        std::size_t arity;
+    };
+
+    struct indexed_arg_factory_t
+    {
+        resolver_t* resolver = nullptr;
+
+        template <std::size_t, std::size_t index>
+        constexpr auto create(auto& resolver) -> auto
+        {
+            this->resolver = &resolver;
+            return arg_t{resolver};
+        }
+    };
+
+    struct instance_factory_t
+    {
+        resolver_t* resolver;
+
+        auto assert_arg(arg_t const& arg) const noexcept -> void { ASSERT_EQ(resolver, &arg.resolver); }
+
+        constexpr auto operator()() -> constructed_t { return constructed_t{0}; }
+
+        constexpr auto operator()(arg_t const& arg) -> constructed_t
+        {
+            assert_arg(arg);
+            return constructed_t{1};
+        }
+
+        constexpr auto operator()(arg_t const& arg1, arg_t const& arg2) -> constructed_t
+        {
+            assert_arg(arg1);
+            assert_arg(arg2);
+            return constructed_t{2};
+        }
+    };
+
+    template <std::size_t arity>
+    using sut_t = arity_dispatcher_t<constructed_t, indexed_arg_factory_t, std::make_index_sequence<arity>>;
+};
+
+TEST_F(factory_invoker_detail_dispatcher_test_t, arity_0_constructs_with_0_args)
+{
+    auto sut = sut_t<0>{};
+    auto instance_factory = instance_factory_t{.resolver = &resolver};
+    auto const constructed = sut(instance_factory, resolver);
+    ASSERT_EQ(0, constructed.arity);
+}
+
+TEST_F(factory_invoker_detail_dispatcher_test_t, arity_1_constructs_with_1_args)
+{
+    auto sut = sut_t<1>{};
+    auto instance_factory = instance_factory_t{.resolver = &resolver};
+    auto const constructed = sut(instance_factory, resolver);
+    ASSERT_EQ(1, constructed.arity);
+}
+
+TEST_F(factory_invoker_detail_dispatcher_test_t, arity_2_constructs_with_2_args)
+{
+    auto sut = sut_t<2>{};
+    auto instance_factory = instance_factory_t{.resolver = &resolver};
+    auto const constructed = sut(instance_factory, resolver);
+    ASSERT_EQ(2, constructed.arity);
+}
+
+} // namespace
+} // namespace factory_invoker::detail
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace {
+
+struct factory_invoker_test_t : Test
+{
+    struct resolver_t
+    {};
+    resolver_t resolver;
+
+    struct arg_t
+    {
+        constexpr explicit arg_t(resolver_t&) {}
+    };
+
+    struct single_arg_t
+    {
+        constexpr explicit single_arg_t(arg_t) {}
+    };
+
+    struct constructed_t
+    {
+        int_t arity = -1;
+    };
+
+    template <std::size_t arity>
+    using sut_t = factory_invoker_t<constructed_t, arity, arg_t, single_arg_t>;
+};
+
+TEST_F(factory_invoker_test_t, invokes_factory_with_zero_args)
+{
+    auto factory = [&]() { return constructed_t{0}; };
+
+    auto const result = sut_t<0>{}(factory, resolver);
+
+    ASSERT_EQ(result.arity, 0);
+}
+
+TEST_F(factory_invoker_test_t, invokes_factory_with_single_arg)
+{
+    auto factory = [&](single_arg_t const& arg) { return constructed_t{1}; };
+
+    auto const result = sut_t<1>{}(factory, resolver);
+
+    ASSERT_EQ(result.arity, 1);
+}
+
+TEST_F(factory_invoker_test_t, invokes_factory_with_multiple_args)
+{
+    auto factory = [&](arg_t const& arg1, arg_t const& arg2) { return constructed_t{2}; };
+
+    auto const result = sut_t<2>{}(factory, resolver);
+
+    ASSERT_EQ(result.arity, 2);
+}
+
+} // namespace
+} // namespace dink
