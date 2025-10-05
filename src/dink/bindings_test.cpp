47d19a6be85d30ccebaaf9832e7ab87bb143dfe8
Frank Secilia <frank.secilia@gmail.com>
2025-10-05 17:58:29 -0400

    bindings: shell module

---
+++ src/dink/bindings_test.cpp
@@ -0,0 +1,15 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#include "bindings.hpp"
+#include <dink/test.hpp>
+
+namespace dink {
+namespace {
+
+//
+
+} // namespace
+} // namespace dink
