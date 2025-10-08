1a8c3b821054bb1759409a4cb6875a91b59c453d
Frank Secilia <frank.secilia@gmail.com>
2025-10-07 21:00:05 -0400

    stage old dink_integration_test.cpp

--- src/dink/dink_integration_test.cpp
+++ src/dink/dink_integration_test.cpp
@@ -9,11 +9,277 @@
 namespace dink {
 namespace {
 
-struct integration_test_t : Test
-{};
+inline static int_t next_id = 1;
 
-TEST_F(integration_test_t, integration_test_ready)
+template <typename... args_t>
+struct constructible_t
 {
+    int_t id = next_id++;
+    constructible_t(args_t...) noexcept {}
+};
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+struct integration_test_t : Test
+{
+    using sut_t = dink_t;
+    sut_t sut{};
+
+    template <typename resolved_t>
+    struct expected_t
+    {
+        using result_t = resolved_t;
+    };
+
+    template <typename resolved_t>
+    struct expected_t<resolved_t&&>
+    {
+        using result_t = resolved_t;
+    };
+
+    template <typename resolved_t>
+    auto resolve() -> decltype(auto)
+    {
+        return static_cast<typename expected_t<resolved_t>::result_t>(sut.template resolve<resolved_t>());
+    }
+
+    template <typename value_t>
+    auto test_resolve()
+    {
+        auto const& value = resolve<value_t>();
+        auto const& ref = resolve<value_t&>();
+        ASSERT_NE(&value, &ref);
+    }
+};
+
+TEST_F(integration_test_t, vals_are_transient)
+{
+    ASSERT_NE(resolve<constructible_t<>>().id, resolve<constructible_t<>&&>().id);
+}
+
+TEST_F(integration_test_t, refs_are_shared)
+{
+    ASSERT_EQ(resolve<constructible_t<>&>().id, resolve<constructible_t<> const&>().id);
+}
+
+TEST_F(integration_test_t, rrefs_are_transient)
+{
+    ASSERT_NE(resolve<constructible_t<>&&>().id, resolve<constructible_t<> const&&>().id);
+}
+
+TEST_F(integration_test_t, default_constructible)
+{
+    test_resolve<constructible_t<>>();
+}
+
+TEST_F(integration_test_t, constructible_from_val)
+{
+    test_resolve<constructible_t<constructible_t<>>>();
+}
+
+TEST_F(integration_test_t, constructible_from_lref)
+{
+    test_resolve<constructible_t<constructible_t<>&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_rref)
+{
+    test_resolve<constructible_t<constructible_t<>&&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_cref)
+{
+    test_resolve<constructible_t<constructible_t<> const&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_rcref)
+{
+    test_resolve<constructible_t<constructible_t<> const&&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_val_val)
+{
+    test_resolve<constructible_t<constructible_t<>, constructible_t<>>>();
+}
+
+TEST_F(integration_test_t, constructible_from_val_lref)
+{
+    test_resolve<constructible_t<constructible_t<>, constructible_t<>&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_val_rref)
+{
+    test_resolve<constructible_t<constructible_t<>, constructible_t<>&&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_val_cref)
+{
+    test_resolve<constructible_t<constructible_t<>, constructible_t<> const&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_val_rcref)
+{
+    test_resolve<constructible_t<constructible_t<>, constructible_t<> const&&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_lref_val)
+{
+    test_resolve<constructible_t<constructible_t<>&, constructible_t<>>>();
+}
+
+TEST_F(integration_test_t, constructible_from_lref_lref)
+{
+    test_resolve<constructible_t<constructible_t<>&, constructible_t<>&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_lref_rref)
+{
+    test_resolve<constructible_t<constructible_t<>&, constructible_t<>&&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_lref_cref)
+{
+    test_resolve<constructible_t<constructible_t<>&, constructible_t<> const&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_lref_rcref)
+{
+    test_resolve<constructible_t<constructible_t<>&, constructible_t<> const&&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_rref_val)
+{
+    test_resolve<constructible_t<constructible_t<>&&, constructible_t<>>>();
+}
+
+TEST_F(integration_test_t, constructible_from_rref_lref)
+{
+    test_resolve<constructible_t<constructible_t<>&&, constructible_t<>&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_rref_rref)
+{
+    test_resolve<constructible_t<constructible_t<>&&, constructible_t<>&&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_rref_cref)
+{
+    test_resolve<constructible_t<constructible_t<>&&, constructible_t<> const&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_rref_rcref)
+{
+    test_resolve<constructible_t<constructible_t<>&&, constructible_t<> const&&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_cref_val)
+{
+    test_resolve<constructible_t<constructible_t<> const&, constructible_t<>>>();
+}
+
+TEST_F(integration_test_t, constructible_from_cref_lref)
+{
+    test_resolve<constructible_t<constructible_t<> const&, constructible_t<>&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_cref_rref)
+{
+    test_resolve<constructible_t<constructible_t<> const&, constructible_t<>&&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_cref_cref)
+{
+    test_resolve<constructible_t<constructible_t<> const&, constructible_t<> const&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_cref_rcref)
+{
+    test_resolve<constructible_t<constructible_t<> const&, constructible_t<> const&&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_rcref_val)
+{
+    test_resolve<constructible_t<constructible_t<> const&&, constructible_t<>>>();
+}
+
+TEST_F(integration_test_t, constructible_from_rcref_lref)
+{
+    test_resolve<constructible_t<constructible_t<> const&&, constructible_t<>&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_rcref_rref)
+{
+    test_resolve<constructible_t<constructible_t<> const&&, constructible_t<>&&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_rcref_cref)
+{
+    test_resolve<constructible_t<constructible_t<> const&&, constructible_t<> const&>>();
+}
+
+TEST_F(integration_test_t, constructible_from_rcref_rcref)
+{
+    test_resolve<constructible_t<constructible_t<> const&&, constructible_t<> const&&>>();
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+struct integration_test_nested_composer_t : integration_test_t
+{
+    struct resolved_t
+    {};
+
+    struct nested_resolved_t
+    {};
+};
+
+TEST_F(integration_test_nested_composer_t, parallel_nested_resolve_independently)
+{
+    dink_t::nested_t nested_composer1 = sut.create_nested();
+    dink_t::nested_t nested_composer2 = sut.create_nested();
+    ASSERT_NE(
+        &nested_composer1.template resolve<nested_resolved_t&>(),
+        &nested_composer2.template resolve<nested_resolved_t&>()
+    );
+}
+
+// vvvvvv -- from here and below, nested_resolved in sut stays resolved until program end, not test teardown -- vvvvvv
+TEST_F(integration_test_nested_composer_t, resolve_nested_first_independent_of_parent)
+{
+    dink_t::nested_t nested_composer = sut.create_nested();
+
+    auto& in_nested = nested_composer.template resolve<nested_resolved_t&>();
+    auto& in_sut = sut.template resolve<nested_resolved_t&>();
+
+    ASSERT_NE(&in_nested, &in_sut);
+}
+
+// vvvvvv -- from here and below, resolved in sut stays resolved until program end, not test teardown -- vvvvvv
+TEST_F(integration_test_nested_composer_t, resolve_parent_first_same_for_both)
+{
+    dink_t::nested_t nested_composer = sut.create_nested();
+
+    auto& in_sut = sut.template resolve<resolved_t&>();
+    auto& in_nested = nested_composer.template resolve<resolved_t&>();
+
+    ASSERT_EQ(&in_nested, &in_sut);
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+struct integration_test_deep_graph_t : integration_test_t
+{
+    template <typename value_t>
+    using nested_t = constructible_t<value_t, value_t&, value_t&&, value_t const, value_t const&, value_t const&&>;
+
+    using val_t = constructible_t<>;
+    using obj_t = nested_t<val_t>;
+    using graph_t = nested_t<obj_t>;
+};
+
+TEST_F(integration_test_deep_graph_t, resolve)
+{
+    test_resolve<graph_t>();
 }
 
 } // namespace
