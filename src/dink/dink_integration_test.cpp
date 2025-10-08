51fddb94de39ae99fc5fb6126b290ca8f80c7cd6
Frank Secilia <frank.secilia@gmail.com>
2025-10-08 06:55:23 -0400

    simplify naming in tests

--- src/dink/dink_integration_test.cpp
+++ src/dink/dink_integration_test.cpp
@@ -13,56 +13,54 @@ namespace {
 inline static int_t next_id = 1;
 
 template <typename... args_t>
-struct instance_t
+struct constructed_from_t
 {
     int_t id = next_id++;
-    instance_t(args_t...) noexcept {}
+    constructed_from_t(args_t...) noexcept {}
 };
 
 struct integration_smoke_test_t : Test
-{
-    using A = instance_t<>;
-    using B = instance_t<A>;
-    using C = instance_t<A, B>;
-};
+{};
 
 TEST_F(integration_smoke_test_t, default_ctor_transient)
 {
-    root_container_t<> container;
+    using instance_t = constructed_from_t<>;
 
-    auto a1 = container.resolve<A>();
-    auto a2 = container.resolve<A>();
+    auto container = root_container_t<>{};
+    auto a1 = container.resolve<instance_t>();
+    auto a2 = container.resolve<instance_t>();
 
     ASSERT_NE(a1.id, a2.id); // Transient - different instances
 }
 
 TEST_F(integration_smoke_test_t, default_ctor_singleton)
 {
-    root_container_t<> container;
+    using instance_t = constructed_from_t<>;
 
-    auto& a1 = container.resolve<A&>();
-    auto& a2 = container.resolve<A&>();
+    auto container = root_container_t<>{};
+    auto& a1 = container.resolve<instance_t&>();
+    auto& a2 = container.resolve<instance_t&>();
 
     ASSERT_EQ(&a1, &a2); // Singleton - same instance
 }
 
 TEST_F(integration_smoke_test_t, dependency_graph)
 {
-    root_container_t<> container;
+    using instance_t = constructed_from_t<constructed_from_t<>, constructed_from_t<constructed_from_t<>>>;
 
-    auto c = container.resolve<C>();
+    auto container = root_container_t<>{};
+    auto c = container.resolve<instance_t>();
 
     EXPECT_TRUE(c.id > 0); // Successfully constructed with dependencies
 }
 
 TEST_F(integration_smoke_test_t, explicit_binding)
 {
-    using namespace dink;
+    using instance_t = constructed_from_t<>;
 
-    root_container_t container{bind<A>().to<A>().in<scopes::singleton_t>()};
-
-    auto a1 = container.resolve<A>();
-    auto a2 = container.resolve<A>();
+    auto container = root_container_t{bind<instance_t>().to<instance_t>().in<scopes::singleton_t>()};
+    auto a1 = container.resolve<instance_t>();
+    auto a2 = container.resolve<instance_t>();
 
     ASSERT_EQ(a1.id, a2.id); // Explicitly bound singleton
 }
