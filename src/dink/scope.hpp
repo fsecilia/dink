b9742553e81312591583274eba91184c5c8c97be
Frank Secilia <frank.secilia@gmail.com>
2025-10-10 16:52:56 -0400

    move scope to module

---
+++ src/dink/scope.hpp
@@ -0,0 +1,162 @@
+/*!
+    \file
+    Copyright (C) 2025 Frank Secilia
+*/
+
+#pragma once
+
+#include <dink/lib.hpp>
+#include <dink/instance_cache.hpp>
+#include <atomic>
+#include <memory>
+#include <mutex>
+#include <utility>
+
+namespace dink {
+
+//! initializes an instance using the double-checked locking pattern
+template <typename instance_t>
+struct double_checked_storage_t
+{
+public:
+    template <typename factory_t>
+    auto get_or_create(factory_t&& factory) -> instance_t&
+    {
+        // try loading outside of lock
+        auto* result = atomic.load(std::memory_order_acquire);
+        if (result != nullptr) return *result;
+
+        std::lock_guard lock(creation_mutex);
+
+        // try loading from inside of lock
+        result = atomic.load(std::memory_order_relaxed);
+        if (result != nullptr) return *result;
+
+        // couldn't get, so create
+        return create(std::forward<factory_t>(factory));
+    }
+
+    auto get_if_initialized() const noexcept -> instance_t* { return atomic.load(std::memory_order_acquire); }
+
+private:
+    // store instance in a union so we can control when ctor and dtor are called without using heap
+    union union_t
+    {
+        union_t() {}
+        ~union_t() {}
+        instance_t instance;
+    };
+    union_t storage;
+
+    // track instance in a unique_ptr with custom deleter for union
+    std::unique_ptr<instance_t, void (*)(instance_t*)> instance{nullptr, [](instance_t* p) { p->~instance_t(); }};
+
+    // instance is replicated into an atomic when valid
+    std::atomic<instance_t*> atomic{nullptr};
+
+    // creation uses the double-checked locking pattern
+    std::mutex creation_mutex;
+
+    //! creates instance under lock, replicates to atomic_instance
+    template <typename factory_t>
+    auto create(factory_t&& factory) -> instance_t&
+    {
+        // find where to put instance
+        auto* storage_ptr = &storage.instance;
+
+        // call ctor
+        new (storage_ptr) instance_t(factory());
+
+        // capture instance
+        instance.reset(storage_ptr);
+
+        // replicate into atomic
+        atomic.store(storage_ptr, std::memory_order_release);
+
+        return *instance;
+    }
+};
+
+//! process-wide accessible type-indexed instance; this is singleton
+template <typename instance_t, typename storage_t = double_checked_storage_t<instance_t>>
+class type_indexed_storage_t
+{
+public:
+    template <typename factory_t>
+    static auto get_or_create(factory_t&& factory) -> instance_t&
+    {
+        return instance().get_or_create(std::forward<factory_t>(factory));
+    }
+
+    static auto get_if_initialized() noexcept -> instance_t* { return instance().get_if_initialized(); }
+
+private:
+    static auto instance() -> double_checked_storage_t<instance_t>&
+    {
+        static storage_t instance;
+        return instance;
+    }
+};
+
+namespace container::scope {
+
+struct global_t
+{
+public:
+    template <typename instance_t, typename dependency_chain_t, typename provider_t, typename container_t>
+    auto resolve_singleton(provider_t& provider, container_t& container) -> instance_t&
+    {
+        return type_indexed_storage_t<instance_t>::get_or_create([&]() {
+            return provider.template create<dependency_chain_t>(container);
+        });
+    }
+
+    template <typename instance_t, typename dependency_chain_t, typename provider_t, typename container_t>
+    auto resolve_shared_ptr(provider_t& provider, container_t& container) -> std::shared_ptr<instance_t>&
+    {
+        // wrap in a shared_ptr with no-op deleter
+        static auto shared = std::shared_ptr<instance_t>{
+            &resolve_singleton<instance_t, dependency_chain_t>(provider, container), [](auto&&) {}
+        };
+        return shared;
+    }
+
+    template <typename instance_t>
+    auto find_in_local_cache() -> instance_t*
+    {
+        return type_indexed_storage_t<instance_t>::get_if_initialized();
+    }
+};
+
+template <typename parent_t>
+struct nested_t
+{
+    parent_t* parent;
+    explicit nested_t(parent_t& parent) : parent{&parent} {}
+
+    dink::instance_cache_t cache;
+
+    template <typename instance_t, typename dependency_chain_t, typename provider_t, typename container_t>
+    auto resolve_singleton(provider_t& provider, container_t& container) -> std::shared_ptr<instance_t>
+    {
+        return cache.template get_or_create<instance_t>([&]() {
+            return provider.template create<dependency_chain_t>(container);
+        });
+    }
+
+    template <typename instance_t, typename dependency_chain_t, typename provider_t, typename container_t>
+    auto resolve_shared_ptr(provider_t& provider, container_t& container) -> std::shared_ptr<instance_t>
+    {
+        // the cache already stores shared_ptrs, so just return that directly
+        return resolve_singleton<instance_t, dependency_chain_t>(provider, container);
+    }
+
+    template <typename resolved_t>
+    auto find_in_local_cache() -> std::shared_ptr<resolved_t>
+    {
+        return cache.template get<resolved_t>();
+    }
+};
+
+} // namespace container::scope
+} // namespace dink
