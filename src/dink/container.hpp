9df132e3b82bbf008411fbb4f7040998bb8edf00
Frank Secilia <frank.secilia@gmail.com>
2025-10-10 05:40:14 -0400

    s/instance_creator/resolver

--- src/dink/container.hpp
+++ src/dink/container.hpp
@@ -169,7 +169,7 @@ private:
 template <typename... bindings_t>
 config_t(bindings_t&&...) -> config_t<std::decay_t<bindings_t>...>;
 
-template <typename scope_t, typename config_t, typename instance_creator_t>
+template <typename scope_t, typename config_t, typename resolver_t>
 class container_t;
 
 // --- Concept to identify a container (avoids ambiguity)
@@ -179,8 +179,8 @@ template <typename>
 struct is_container_f : std::false_type
 {};
 
-template <typename scope_t, typename config_t, typename instance_creator_t>
-struct is_container_f<container_t<scope_t, config_t, instance_creator_t>> : std::true_type
+template <typename scope_t, typename config_t, typename resolver_t>
+struct is_container_f<container_t<scope_t, config_t, resolver_t>> : std::true_type
 {};
 
 } // namespace detail
@@ -188,7 +188,7 @@ struct is_container_f<container_t<scope_t, config_t, instance_creator_t>> : std:
 template <typename T> concept is_container = detail::is_container_f<std::decay_t<T>>::value;
 
 // Creates an instance from a provider, handling lifecycle and caching.
-class instance_creator_t
+class resolver_t
 {
 public:
     // create instance from a binding
@@ -281,7 +281,7 @@ struct config_from_tuple_f<tuple_p<bindings_t...>>
     using type = config_t<bindings_t...>;
 };
 
-template <typename scope_t, typename config_t, typename instance_creator_t>
+template <typename scope_t, typename config_t, typename resolver_t>
 class container_t : public scope_t
 {
     // Helper to get the deduced config type from a set of configs
@@ -292,8 +292,8 @@ class container_t : public scope_t
 public:
     inline static constexpr auto is_global = std::same_as<scope_t, container::scope::global_t>;
 
-    container_t(scope_t scope, config_t config, instance_creator_t instance_creator) noexcept
-        : scope_t{std::move(scope)}, config_{std::move(config)}, instance_creator_{std::move(instance_creator)}
+    container_t(scope_t scope, config_t config, resolver_t resolver) noexcept
+        : scope_t{std::move(scope)}, config_{std::move(config)}, resolver_{std::move(resolver)}
     {}
 
     // global constructor
@@ -314,7 +314,7 @@ public:
     {
         // run search for instance or bindings up the chain, or use the default provider with this container.
         return try_resolve<request_t, dependency_chain_t>([this]() -> decltype(auto) {
-            return instance_creator_.template create_from_default_provider<request_t, dependency_chain_t>(*this);
+            return resolver_.template create_from_default_provider<request_t, dependency_chain_t>(*this);
         });
     }
 
@@ -339,7 +339,7 @@ public:
             static constexpr auto binding_found = !std::same_as<binding_t, binding_not_found_t>;
             if constexpr (binding_found)
             {
-                return instance_creator_.template create_from_binding<request_t, dependency_chain_t>(*binding, *this);
+                return resolver_.template create_from_binding<request_t, dependency_chain_t>(*binding, *this);
             }
         }
 
@@ -357,7 +357,7 @@ public:
 
 private:
     config_t config_{};
-    instance_creator_t instance_creator_{};
+    resolver_t resolver_{};
 };
 
 /*
@@ -384,29 +384,28 @@ container_t(first_binding_p&&, rest_bindings_p&&...) -> container_t<
     container::scope::global_t,
     typename config_from_tuple_f<
         decltype(resolve_bindings(std::declval<first_binding_p>(), std::declval<rest_bindings_p>()...))>::type,
-    instance_creator_t>;
+    resolver_t>;
 
 //! deduction guide for empty global containers
-container_t() -> container_t<container::scope::global_t, config_t<>, instance_creator_t>;
+container_t() -> container_t<container::scope::global_t, config_t<>, resolver_t>;
 
 //! deduction guide for nested containers
 template <typename p_scope_t, typename p_config_t, typename p_creator_t, typename... bindings_t>
 requires(is_binding<bindings_t> && ...)
 container_t(container_t<p_scope_t, p_config_t, p_creator_t>& parent, bindings_t&&...) -> container_t<
     container::scope::nested_t<std::decay_t<decltype(parent)>>,
-    typename config_from_tuple_f<decltype(resolve_bindings(std::declval<bindings_t>()...))>::type, instance_creator_t>;
+    typename config_from_tuple_f<decltype(resolve_bindings(std::declval<bindings_t>()...))>::type, resolver_t>;
 
 // type aliases
 
 template <typename... bindings_t>
 using global_container_t = container_t<
     container::scope::global_t,
-    typename config_from_tuple_f<decltype(resolve_bindings(std::declval<bindings_t>()...))>::type, instance_creator_t>;
+    typename config_from_tuple_f<decltype(resolve_bindings(std::declval<bindings_t>()...))>::type, resolver_t>;
 
 template <typename parent_t, typename... bindings_t>
 using child_container_t = container_t<
     container::scope::nested_t<parent_t>,
-    typename config_from_tuple_f<decltype(resolve_bindings(std::declval<bindings_t>()...))>::type,
-    dink::instance_creator_t>;
+    typename config_from_tuple_f<decltype(resolve_bindings(std::declval<bindings_t>()...))>::type, dink::resolver_t>;
 
 } // namespace dink
