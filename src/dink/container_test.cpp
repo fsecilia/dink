276a4e69d521745f61b17b4b7c336e944e9bfdcb
Frank Secilia <frank.secilia@gmail.com>
2025-10-12 20:42:59 -0400

    start on mocks for container, get preempted

--- src/dink/container_test.cpp
+++ src/dink/container_test.cpp
@@ -9,7 +9,30 @@
 namespace dink {
 namespace {
 
-//
+struct container_test_t
+{
+    struct mock_cache_t
+    {
+        virtual ~mock_cache_t() = default;
+    };
+    StrictMock<mock_cache_t> mock_cache;
+
+    struct cache_t
+    {
+        mock_cache_t* mock = nullptr;
+    };
+
+    struct mock_delegate_t
+    {
+        virtual ~mock_delegate_t() = default;
+    };
+    StrictMock<mock_delegate_t> mock_delegate;
+
+    struct delegate_t
+    {
+        mock_delegate_t* mock_delegate = nullptr;
+    };
+};
 
 } // namespace
 } // namespace dink
