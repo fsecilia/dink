707a2b80da8de9686a1e3b81bf4a2f4149c16867
Frank Secilia <frank.secilia@gmail.com>
2025-10-15 23:46:32 -0400

    bash stale generic creator provider

--- src/dink/provider.hpp
+++ src/dink/provider.hpp
@@ -54,28 +54,6 @@ struct factory_invoker_t {
     }
 };
 
-//! unified creator provider - handles both ctors and user factories
-template <typename constructed_t, typename factory_t = ctor_factory_t<constructed_t>>
-struct creator_t {
-    using default_scope_t = scope::transient_t;
-    using provided_t      = constructed_t;
-
-    [[no_unique_address]] factory_t factory;
-
-    template <typename dependency_chain_t, typename container_t>
-    auto create(container_t& container) -> constructed_t {
-        using arg_t                 = arg_t<container_t, dependency_chain_t>;
-        using single_arg_t          = single_arg_t<constructed_t, arg_t>;
-        static constexpr auto arity = arity_v<constructed_t, factory_t>;
-        return create<dependency_chain_t>(container, invoker_t<constructed_t, arity, arg_t, single_arg_t>{});
-    }
-
-    template <typename dependency_chain_t, typename container_t, typename factory_invoker_t>
-    auto create(container_t& container, factory_invoker_t&& factory_invoker) -> constructed_t {
-        return std::forward<factory_invoker_t>(factory_invoker).invoke_factory(factory, container);
-    }
-};
-
 //! references an instance owned by the container (moved/copied in)
 template <typename instance_t>
 struct internal_reference_t {
@@ -129,15 +107,6 @@ struct provider_factory_t {
 
 //! default factory creates default providers
 using default_factory_t = provider_factory_t<default_t>;
-
-template <typename value_t>
-struct is_creator_f : std::false_type {};
-
-template <typename constructed_t, typename factory_t>
-struct is_creator_f<creator_t<constructed_t, factory_t>> : std::true_type {};
-
-template <typename value_t>
-inline static constexpr auto is_creator_v = is_creator_f<value_t>::value;
 
 template <typename provider_t>
 concept is_creator = requires(provider_t& provider, meta::concept_probe_t& container) {
