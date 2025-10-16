1de27d40c07f6a390adb5b5ce011c1dccf319193
Frank Secilia <frank.secilia@gmail.com>
2025-10-15 22:43:44 -0400

    arity checks now investigate ctors directly

--- src/dink/arity.hpp
+++ src/dink/arity.hpp
@@ -42,8 +42,44 @@ template <std::size_t index>
 using args_t = meta::indexed_type_t<probe_arg_t, index>;
 
 /*!
-    investigates factory_t's call operators, looking for an overload with an arity based on the length of
-    `index_sequence_t` that returns a type convertible to constructed_t, then checks the next smaller, all the way to 0
+    resolves invoke_result_t only if is_invocable is already known to be true
+
+    The expression std::is_invocable_v<factory_t, args_t...>
+    && std::is_same<constructed_t, std::invoke_result_t<factory_t, args_t...>> is invalid if the factory is not
+    invocable with those args. dependent_invoke_result_f produces the same logical expression, but only evaluates
+    invoke_result_t after is_invocable_v is known to be true.
+*/
+template <bool is_invocable, typename constructed_t, typename factory_t, typename... args_t>
+struct dependent_invoke_result_f : std::false_type {};
+
+//! specialization dependent on is_invocable already being true
+template <typename constructed_t, typename factory_t, typename... args_t>
+struct dependent_invoke_result_f<true, constructed_t, factory_t, args_t...>
+    : std::is_same<constructed_t, std::invoke_result_t<factory_t, args_t...>> {};
+
+//! probes arity in factory call operators that return constructed_t, or ctors of constructed_t
+template <typename constructed_t, typename factory_t, typename... args_t>
+struct arity_probe_t;
+
+//! generic parameterization matches a factory's call operator that returns an instance of constructed_t
+template <typename constructed_t, typename factory_t, typename... args_t>
+struct arity_probe_t {
+    static constexpr auto matches = dependent_invoke_result_f<std::is_invocable_v<factory_t, args_t...>, constructed_t,
+                                                              factory_t, args_t...>::value;
+};
+
+//! specialization for void factory matches constructed_t's ctor directly
+template <typename constructed_t, typename... args_t>
+struct arity_probe_t<constructed_t, void, args_t...> {
+    static constexpr auto matches = std::is_constructible_v<constructed_t, args_t...>;
+};
+
+/*!
+    probes arity of factory call operators or constructors to find the greediest overload
+
+    This metafunction uses a generic arity probe to investigate either the call operators of a factory or the ctors of
+    the constructed type. It looks for a matching overload with an arity based on the length of `index_sequence_t`,
+    then checks the next smaller, all the way to 0.
 */
 template <typename constructed_t, typename factory_t, typename index_sequence_t>
 struct arity_f;
@@ -51,49 +87,34 @@ struct arity_f;
 //! general case checks current arity then advances to next smaller
 template <typename constructed_t, typename factory_t, std::size_t index, std::size_t... remaining_indices>
 struct arity_f<constructed_t, factory_t, std::index_sequence<index, remaining_indices...>> {
-    static constexpr std::size_t const value = []() noexcept -> std::size_t {
-        using next_arity_f = arity_f<constructed_t, factory_t, std::index_sequence<remaining_indices...>>;
+    using next_arity_f = arity_f<constructed_t, factory_t, std::index_sequence<remaining_indices...>>;
+    static constexpr std::size_t cur_arity = sizeof...(remaining_indices) + 1;
+    using cur_arg_t = std::conditional_t<cur_arity == 1, single_probe_arg_t<constructed_t>, probe_arg_t>;
 
-        constexpr auto cur_arity = sizeof...(remaining_indices) + 1;
-        using cur_arg_t          = std::conditional_t<cur_arity == 1, single_probe_arg_t<constructed_t>, probe_arg_t>;
-        if constexpr (std::is_invocable_v<factory_t, cur_arg_t, args_t<remaining_indices>...>) {
-            if constexpr (std::is_same_v<constructed_t,
-                                         std::invoke_result_t<factory_t, cur_arg_t, args_t<remaining_indices>...>>) {
-                return cur_arity;
-            } else {
-                return next_arity_f::value;
-            }
-        } else {
-            return next_arity_f::value;
-        }
-    }();
+    static constexpr std::size_t value =
+        arity_probe_t<constructed_t, factory_t, cur_arg_t, args_t<remaining_indices>...>::matches ? cur_arity
+                                                                                                  : next_arity_f::value;
 };
 
 //! base case checks 0-arity or resolves to npos
 template <typename constructed_t, typename factory_t>
 struct arity_f<constructed_t, factory_t, std::index_sequence<>> {
-    static constexpr std::size_t const value = []() noexcept -> std::size_t {
-        if constexpr (std::is_invocable_v<factory_t>) {
-            if constexpr (std::is_convertible_v<constructed_t, std::invoke_result_t<factory_t>>) {
-                return 0;
-            } else {
-                return npos;
-            }
-        } else {
-            return npos;
-        }
-    }();
+    static constexpr std::size_t value = arity_probe_t<constructed_t, factory_t>::matches ? 0 : npos;
 };
 
 }  // namespace detail
 }  // namespace arity
 
 /*!
-    searches `factory_t` to find arity of greediest call operator that returns a type convertible to `constructed_t`
+    searches factory for largest arity call that produces constructed, or searches constructed's ctors directly
+
+    If `factory_t` is a callable type, `arity_v` investigates its call operators and resolves to the largest arity that
+    returns a type converible to `constructed_t`. If `factory_t` is `void`, `arity_v` investigates `constructed_t`'s
+    constructors directly instead.
 
     resolves to discovered arity or `npos`
 */
-template <typename constructed_t, typename factory_t = ctor_factory_t<constructed_t>>
+template <typename constructed_t, typename factory_t = void>
 inline constexpr auto arity_v =
     arity::detail::arity_f<constructed_t, factory_t, std::make_index_sequence<dink_max_deduced_arity>>::value;
 
--- src/dink/arity_test.cpp
+++ src/dink/arity_test.cpp
@@ -11,7 +11,7 @@ namespace {
 
 template <typename... args_t>
 struct constructed_by_t {
-    constructed_by_t(args_t...) noexcept {}
+    constructed_by_t(args_t...) noexcept;
 };
 
 static_assert(arity_v<constructed_by_t<>> == 0);
@@ -31,6 +31,27 @@ struct multi_ctor_t {
 
 // makes sure search is greedy
 static_assert(arity_v<multi_ctor_t> == 3);
+
+template <typename... args_t>
+struct factory_t {
+    // matching call operator
+    auto operator()(args_t...) -> constructed_by_t<args_t...>;
+
+    // confounding overloaded call operator
+    auto operator()(bool, args_t...) -> bool;
+};
+
+static_assert(arity_v<constructed_by_t<>, factory_t<>> == 0);
+static_assert(arity_v<constructed_by_t<bool>, factory_t<bool>> == 1);
+static_assert(arity_v<constructed_by_t<int, bool>, factory_t<int, bool>> == 2);
+static_assert(arity_v<constructed_by_t<void*, int, bool>, factory_t<void*, int, bool>> == 3);
+static_assert(arity_v<constructed_by_t<float&, void*, int, bool>, factory_t<float&, void*, int, bool>> == 4);
+static_assert(arity_v<constructed_by_t<constructed_by_t<>&&, float&, void*, int, bool>,
+                      factory_t<constructed_by_t<>&&, float&, void*, int, bool>> == 5);
+static_assert(arity_v<constructed_by_t<bool, int, void const*, float const&, constructed_by_t<>&&>,
+                      factory_t<bool, int, void const*, float const&, constructed_by_t<>&&>> == 5);
+static_assert(arity_v<constructed_by_t<int, int, int, int, int, int, int, int, int, int>,
+                      factory_t<int, int, int, int, int, int, int, int, int, int>> == 10);
 
 }  // namespace
 }  // namespace dink
