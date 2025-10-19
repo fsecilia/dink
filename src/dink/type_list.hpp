4687b249f62a338682a62dec2edcbc88333c90f2
Frank Secilia <frank.secilia@gmail.com>
2025-10-19 16:41:02 -0400

    feat: add TypeList

---
+++ src/dink/type_list.hpp
@@ -0,0 +1,15 @@
+// \file
+// Copyright (c) 2025 Frank Secilia
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <dink/lib.hpp>
+
+namespace dink {
+
+//! Lightweight, compile-time tuple.
+template <typename... Types>
+struct TypeList {};
+
+}  // namespace dink
---
+++ src/dink/type_list_test.cpp
@@ -0,0 +1,14 @@
+// \file
+// Copyright (c) 2025 Frank Secilia
+// SPDX-License-Identifier: MIT
+
+#include "type_list.hpp"
+#include <dink/test.hpp>
+
+namespace dink {
+namespace {
+
+//
+
+}  // namespace
+}  // namespace dink
