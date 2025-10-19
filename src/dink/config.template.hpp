b00940c83e1c250ec57b33c0af24df36dc94533b
Frank Secilia <frank.secilia@gmail.com>
2025-10-19 03:42:23 -0400

    build: add generated cmake config header

---
+++ src/dink/config.template.hpp
@@ -0,0 +1,15 @@
+/*!
+  \file
+  Copyright (C) 2025 Frank Secilia
+*/
+
+#pragma once
+
+// clang-format off
+
+#define dink_version_major ${dink_version_major}
+#define dink_version_minor ${dink_version_minor}
+#define dink_version_patch ${dink_version_patch}
+#define dink_version       "${dink_version}"
+
+// clang-format on
