dcf5e42e83205be4a275ebb90d095a7d14428939
Frank Secilia <frank.secilia@gmail.com>
2025-10-19 19:30:52 -0400

    feat: add dink_no_unique_address

--- src/dink/lib.hpp
+++ src/dink/lib.hpp
@@ -10,6 +10,29 @@
 namespace dink {
 
 // ----------------------------------------------------------------------------
+// dink_no_unique_address
+// ----------------------------------------------------------------------------
+
+// clang-format off
+
+#if !defined dink_no_unique_address
+  #if __has_cpp_attribute(no_unique_address) >= 201803L
+    // __has_cpp_attribute works on all supported compilers if the version is
+    // 201803L or newer.
+    #define dink_no_unique_address no_unique_address
+  #elif !defined __clang__ && defined _MSC_VER && _MSC_VER >= 1929
+    // MSVC first defined their own name for it in v1929, but there is no
+    // support from __has_cpp_attribute.
+    #define dink_no_unique_address msvc::no_unique_address
+  #else
+    // It is unsupported otherwise.
+    #define dink_no_unique_address
+  #endif
+#endif
+
+// clang-format on
+
+// ----------------------------------------------------------------------------
 // processor word size ints
 //
 // Default integer types are the same size as the processor word.
