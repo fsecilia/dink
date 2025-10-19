cdb6e604bc5e66864a568b38a11e8039b32b46c1
Frank Secilia <frank.secilia@gmail.com>
2025-10-19 09:24:10 -0400

    feat: add version module

---
+++ src/dink/version.hpp
@@ -0,0 +1,43 @@
+// \file
+// Copyright (c) 2025 Frank Secilia
+
+#pragma once
+
+#include <dink/lib.hpp>
+#include <dink/version.hpp>
+#include <string>
+
+namespace dink {
+
+//! Lightweight semver encapsulation.
+class Version {
+ public:
+  constexpr auto major() const noexcept -> int_t { return major_; }
+  constexpr auto major(int_t major) noexcept -> void { major_ = major; }
+  constexpr auto minor() const noexcept -> int_t { return minor_; }
+  constexpr auto minor(int_t minor) noexcept -> void { minor_ = minor; }
+  constexpr auto patch() const noexcept -> int_t { return patch_; }
+  constexpr auto patch(int_t patch) noexcept -> void { patch_ = patch; }
+
+  constexpr auto to_string() const noexcept -> std::string {
+    return std::to_string(major_) + "." + std::to_string(minor_) + "." +
+           std::to_string(patch_);
+  }
+
+  constexpr auto operator<=>(Version const& src) const noexcept
+      -> auto = default;
+
+  constexpr Version(int_t major, int_t minor, int_t patch) noexcept
+      : major_{major}, minor_{minor}, patch_{patch} {}
+
+ private:
+  int_t major_{};
+  int_t minor_{};
+  int_t patch_{};
+};
+
+constexpr auto version() noexcept -> Version {
+  return Version{dink_version_major, dink_version_minor, dink_version_patch};
+}
+
+}  // namespace dink
---
+++ src/dink/version_test.cpp
@@ -0,0 +1,33 @@
+// \file
+// Copyright (c) 2025 Frank Secilia
+
+#include "version.hpp"
+#include <dink/test.hpp>
+
+namespace dink {
+namespace {
+
+struct VersionTest : Test {
+  using Sut = Version;
+  Sut const sut =
+      Version{dink_version_major, dink_version_minor, dink_version_patch};
+};
+
+TEST_F(VersionTest, version_matches_preprocessor) {
+  auto const expected = version();
+
+  auto const actual = sut;
+
+  ASSERT_EQ(expected, actual);
+}
+
+TEST_F(VersionTest, version_string_matches_preprocessor) {
+  auto const expected = dink_version;
+
+  auto const actual = sut.to_string();
+
+  ASSERT_EQ(expected, actual);
+}
+
+}  // namespace
+}  // namespace dink
