99f54ed78cb234f8d6819625c95cb0657e4a3d25
Frank Secilia <frank.secilia@gmail.com>
2025-10-19 23:27:01 -0400

    feat: add kDependentBool

--- src/dink/meta.hpp
+++ src/dink/meta.hpp
@@ -8,6 +8,12 @@
 
 namespace dink {
 
+//! constexpr bool, but dependent on a template parameter to delay evaluation.
 //
+// This is useful to trigger a static assert conditionally, using Context to
+// make the expression dependent. The static assert message will also contain
+// information related to Context.
+template <bool condition, typename Context>
+constexpr auto kDependentBool = condition;
 
 }  // namespace dink
