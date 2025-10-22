f35a815bf9e6dd5a7d177f87614a3992d19c8ba6
Frank Secilia <frank.secilia@gmail.com>
2025-10-22 03:07:43 -0400

    feat(scope): add singleton scope

--- src/dink/scope.hpp
+++ src/dink/scope.hpp
@@ -18,4 +18,15 @@ namespace dink::scope {
 // requires it, such as requesting a reference or pointer.
 struct transient_t {};
 
+//! nominally resolves shared instances per request
+//
+// Instances resolved from singleton scope are normally cached by the
+// requesting container, meaning they are constructed from a provider once,
+// saved in the local cache, and the cached instance is returned with reference
+// semantics. A request for a singleton instance can be relegated to transient
+// scope if the type of the request requires it, such as requesting a
+// unique_ptr or rvalue reference. Relegated requests are initialized with a
+// copy of the cached singleton.
+struct singleton_t {};
+
 }  // namespace dink::scope
