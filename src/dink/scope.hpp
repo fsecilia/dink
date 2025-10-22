644572ecf2e221e0a6e02e5405828a544797ed4a
Frank Secilia <frank.secilia@gmail.com>
2025-10-22 03:07:28 -0400

    feat(scope): add transient scope

--- src/dink/scope.hpp
+++ src/dink/scope.hpp
@@ -9,6 +9,13 @@
 
 namespace dink::scope {
 
+//! nominally resolves new instances per request
 //
+// Instances resolved from transient scope are normally created per request,
+// meaning a unique instance is constructed from a provider for every request,
+// and the instance is returned with value semantics. A request for a transient
+// instance can be promoted to singleton scope if the type of the request
+// requires it, such as requesting a reference or pointer.
+struct transient_t {};
 
 }  // namespace dink::scope
