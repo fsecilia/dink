69eacf7caaa4e896d6c6ce5e78a5d60688d6c7d9
Frank Secilia <frank.secilia@gmail.com>
2025-10-22 15:12:27 -0400

    feat(invoker): add SequencedInvoker

--- src/dink/invoker.hpp
+++ src/dink/invoker.hpp
@@ -8,6 +8,8 @@
 #include <dink/arity.hpp>
 #include <dink/resolver.hpp>
 #include <dink/scope.hpp>
+#include <memory>
+#include <utility>
 
 namespace dink {
 
@@ -25,6 +27,76 @@ struct IndexedResolverFactory {
     else
       return Resolver{container};
   }
+};
+
+//! Invokes a ctor or factory by replacing an index sequence.
+//
+// SequencedInvoker replaces each value in an index sequence with the output of
+// an indexed factory, then uses the replaced sequence to either invoke
+// Constructed's ctor, or invoke a factory that produces a Constructed, passed
+// as an argument.
+template <typename Constructed, typename IndexedFactory, typename IndexSequence>
+class SequencedInvoker;
+
+//! Partial specialization to unpack the index sequence.
+template <typename Constructed, typename IndexedFactory, std::size_t... indices>
+class SequencedInvoker<Constructed, IndexedFactory,
+                       std::index_sequence<indices...>> {
+ public:
+  // Constructor Invokers
+  // --------------------------------------------------------------------------
+  constexpr auto create_value(auto& container) const -> Constructed {
+    return Constructed{
+        indexed_factory_.template create<sizeof...(indices), indices>(
+            container)...};
+  }
+
+  constexpr auto create_shared(auto& container) const
+      -> std::shared_ptr<Constructed> {
+    return std::make_shared<Constructed>(
+        indexed_factory_.template create<sizeof...(indices), indices>(
+            container)...);
+  }
+
+  constexpr auto create_unique(auto& container) const
+      -> std::unique_ptr<Constructed> {
+    return std::make_unique<Constructed>(
+        indexed_factory_.template create<sizeof...(indices), indices>(
+            container)...);
+  }
+
+  // Factory Invokers
+  // --------------------------------------------------------------------------
+  constexpr auto create_value(auto& container, auto& constructed_factory) const
+      -> Constructed {
+    return constructed_factory(
+        indexed_factory_.template create<sizeof...(indices), indices>(
+            container)...);
+  }
+
+  constexpr auto create_shared(auto& container, auto& constructed_factory) const
+      -> std::shared_ptr<Constructed> {
+    return std::make_shared<Constructed>(constructed_factory(
+        indexed_factory_.template create<sizeof...(indices), indices>(
+            container)...));
+  }
+
+  constexpr auto create_unique(auto& container, auto& constructed_factory) const
+      -> std::unique_ptr<Constructed> {
+    return std::make_unique<Constructed>(constructed_factory(
+        indexed_factory_.template create<sizeof...(indices), indices>(
+            container)...));
+  }
+
+  // Constructors
+  // --------------------------------------------------------------------------
+  explicit constexpr SequencedInvoker(IndexedFactory indexed_factory) noexcept
+      : indexed_factory_{std::move(indexed_factory)} {}
+
+  SequencedInvoker() = default;
+
+ private:
+  IndexedFactory indexed_factory_{};
 };
 
 }  // namespace dink
--- src/dink/invoker_test.cpp
+++ src/dink/invoker_test.cpp
@@ -5,7 +5,7 @@
 #include "invoker.hpp"
 #include <dink/test.hpp>
 #include <concepts>
-#include <utility>
+#include <tuple>
 
 namespace dink {
 namespace {
@@ -53,6 +53,118 @@ struct IndexedResolverFactoryTest {
 };
 [[maybe_unused]] constexpr auto indexed_resolver_factory_test =
     IndexedResolverFactoryTest{};
+
+// ----------------------------------------------------------------------------
+// SequencedInvoker
+// ----------------------------------------------------------------------------
+
+struct SequencedInvokerFixture {
+  struct Container {};
+
+  // IndexedFactory and Constructed work together so the factory can just
+  // return indices directly instead of indexed resolvers that convert to ctor
+  // paramters that happen to be indices. It's all transparent to
+  // SequencedInvoker, which just replaces a sequence with the results of the
+  // IndexedFactory.
+
+  struct IndexedFactory {
+    template <std::size_t arity, std::size_t index>
+    constexpr auto create(auto& /*container*/) const noexcept -> std::size_t {
+      return index;
+    }
+  };
+
+  template <typename... Args>
+  struct Constructed {
+    std::tuple<Args...> args_tuple;
+    explicit constexpr Constructed(Args... args) noexcept
+        : args_tuple{args...} {}
+  };
+
+  static constexpr auto instance_factory = [](auto&&... args) noexcept {
+    return Constructed{std::forward<decltype(args)>(args)...};
+  };
+
+  template <std::size_t... indices>
+  using Sut =
+      dink::SequencedInvoker<Constructed<decltype(indices)...>, IndexedFactory,
+                             std::index_sequence<indices...>>;
+};
+
+struct SequencedInvokerTestCompileTime : SequencedInvokerFixture {
+  template <std::size_t... indices>
+  static constexpr auto test(auto&& invoke) -> bool {
+    Container container;
+    Sut<indices...> invoker{IndexedFactory{}};
+    auto res = std::forward<decltype(invoke)>(invoke)(container, invoker);
+    return res.args_tuple == std::make_tuple(indices...);
+  }
+
+  template <std::size_t... indices>
+  static constexpr auto test_ctor() -> bool {
+    return test<indices...>([&](auto& container, auto& invoker) constexpr {
+      return invoker.create_value(container);
+    });
+  }
+
+  template <std::size_t... indices>
+  static constexpr auto test_factory() -> bool {
+    return test<indices...>([&](auto& container, auto& invoker) constexpr {
+      return invoker.create_value(container, instance_factory);
+    });
+  }
+
+  static constexpr auto test_arity_0_ctor() -> bool { return test_ctor<>(); }
+  static constexpr auto test_arity_1_ctor() -> bool { return test_ctor<0>(); }
+  static constexpr auto test_arity_3_ctor() -> bool {
+    return test_ctor<0, 1, 2>();
+  }
+
+  static constexpr auto test_arity_0_factory() -> bool {
+    return test_factory<>();
+  }
+
+  static constexpr auto test_arity_1_factory() -> bool {
+    return test_factory<0>();
+  }
+
+  static constexpr auto test_arity_3_factory() -> bool {
+    return test_factory<0, 1, 2>();
+  }
+
+  static constexpr auto instantiate_constexpr_tests() -> void {
+    static_assert(test_arity_0_ctor(), "Arity 0 (Ctor) Failed");
+    static_assert(test_arity_1_ctor(), "Arity 1 (Ctor) Failed");
+    static_assert(test_arity_3_ctor(), "Arity 3 (Ctor) Failed");
+
+    static_assert(test_arity_0_factory(), "Arity 0 (Factory) Failed");
+    static_assert(test_arity_1_factory(), "Arity 1 (Factory) Failed");
+    static_assert(test_arity_3_factory(), "Arity 3 (Factory) Failed");
+  }
+
+  constexpr SequencedInvokerTestCompileTime() noexcept {
+    instantiate_constexpr_tests();
+  }
+};
+[[maybe_unused]] constexpr auto sequenced_invoker_test_constexpr =
+    SequencedInvokerTestCompileTime{};
+
+// ----------------------------------------------------------------------------
+
+struct SequencedInvokerTestRunTime : SequencedInvokerFixture, Test {
+  Container container;
+  Sut<0, 1, 2> sut{IndexedFactory{}};
+};
+
+TEST_F(SequencedInvokerTestRunTime, Arity3SharedPtrCtor) {
+  auto res = sut.create_shared(container);
+  EXPECT_EQ(res->args_tuple, std::make_tuple(0, 1, 2));
+}
+
+TEST_F(SequencedInvokerTestRunTime, Arity3UniquePtrFactory) {
+  auto res = sut.create_unique(container, instance_factory);
+  EXPECT_EQ(res->args_tuple, std::make_tuple(0, 1, 2));
+}
 
 }  // namespace
 }  // namespace dink
