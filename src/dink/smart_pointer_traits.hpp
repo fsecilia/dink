730e084d9428a33a4215838ee239cf6fc6661773
Frank Secilia <frank.secilia@gmail.com>
2025-10-21 20:30:05 -0400

    feat(smp): shell smart pointer traits

---
+++ src/dink/smart_pointer_traits.hpp
@@ -0,0 +1,15 @@
+// \file
+// Copyright (c) 2025 Frank Secilia
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <dink/lib.hpp>
+#include <memory>
+#include <type_traits>
+
+namespace dink {
+
+//
+
+}  // namespace dink
---
+++ src/dink/smart_pointer_traits_test.cpp
@@ -0,0 +1,14 @@
+// \file
+// Copyright (c) 2025 Frank Secilia
+// SPDX-License-Identifier: MIT
+
+#include "smart_pointer_traits.hpp"
+#include <dink/test.hpp>
+
+namespace dink {
+namespace {
+
+//
+
+}  // namespace
+}  // namespace dink
