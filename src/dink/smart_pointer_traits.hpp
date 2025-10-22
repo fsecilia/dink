d63b1f898f6a09b28368d1c5c100abf9c29c389b
Frank Secilia <frank.secilia@gmail.com>
2025-10-21 21:03:30 -0400

    feat(smp): add UniquePtr

--- src/dink/smart_pointer_traits.hpp
+++ src/dink/smart_pointer_traits.hpp
@@ -10,6 +10,20 @@
 
 namespace dink {
 
-//
+// ----------------------------------------------------------------------------
+// unique_ptr
+// ----------------------------------------------------------------------------
+
+template <typename>
+struct IsUniquePtr : std::false_type {};
+
+template <typename Element, typename Deleter>
+struct IsUniquePtr<std::unique_ptr<Element, Deleter>> : std::true_type {};
+
+template <typename Type>
+constexpr bool is_unique_ptr = IsUniquePtr<Type>::value;
+
+template <typename Type>
+concept UniquePtr = is_unique_ptr<std::remove_cvref_t<Type>>;
 
 }  // namespace dink
--- src/dink/smart_pointer_traits_test.cpp
+++ src/dink/smart_pointer_traits_test.cpp
@@ -8,7 +8,44 @@
 namespace dink {
 namespace {
 
-//
+struct Element {};
+struct Deleter {};
+
+using SharedPtr = std::shared_ptr<Element>;
+using WeakPtr = std::weak_ptr<Element>;
+using UniquePtr = std::unique_ptr<Element, Deleter>;
+
+// ----------------------------------------------------------------------------
+// unique_ptr
+// ----------------------------------------------------------------------------
+
+// Trait Variable Template
+// ----------------------------------------------------------------------------
+static_assert(!is_unique_ptr<void>);
+static_assert(!is_unique_ptr<Element>);
+static_assert(!is_unique_ptr<Element*>);
+static_assert(!is_unique_ptr<SharedPtr>);
+static_assert(is_unique_ptr<UniquePtr>);
+static_assert(!is_unique_ptr<WeakPtr>);
+
+static_assert(!is_unique_ptr<const UniquePtr>);
+static_assert(!is_unique_ptr<UniquePtr&>);
+static_assert(!is_unique_ptr<const UniquePtr&>);
+static_assert(!is_unique_ptr<UniquePtr&&>);
+
+// Concept
+// ----------------------------------------------------------------------------
+static_assert(!dink::UniquePtr<void>);
+static_assert(!dink::UniquePtr<Element>);
+static_assert(!dink::UniquePtr<Element*>);
+static_assert(!dink::UniquePtr<SharedPtr>);
+static_assert(dink::UniquePtr<UniquePtr>);
+static_assert(!dink::UniquePtr<WeakPtr>);
+
+static_assert(dink::UniquePtr<const UniquePtr>);
+static_assert(dink::UniquePtr<UniquePtr&>);
+static_assert(dink::UniquePtr<const UniquePtr&>);
+static_assert(dink::UniquePtr<UniquePtr&&>);
 
 }  // namespace
 }  // namespace dink
