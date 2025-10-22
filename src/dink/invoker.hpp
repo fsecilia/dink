24de0c2440b7a18a30c1f132a4f8b364dcbd9de8
Frank Secilia <frank.secilia@gmail.com>
2025-10-22 12:15:59 -0400

    feat(invoker): add IndexedResolverFactory

--- src/dink/invoker.hpp
+++ src/dink/invoker.hpp
@@ -5,9 +5,26 @@
 #pragma once
 
 #include <dink/lib.hpp>
+#include <dink/arity.hpp>
+#include <dink/resolver.hpp>
+#include <dink/scope.hpp>
 
 namespace dink {
 
-//
+//! Factory that consumes indices to produce Resolvers.
+template <typename Resolver, typename SingleArgResolver>
+struct IndexedResolverFactory {
+  //! Creates a resolver, choosing the return type based on arity.
+  //
+  // For arity 1, this returns a \c SingleArgResolver. For all other
+  // arities, it returns a \c Resolver.
+  template <std::size_t arity, std::size_t index>
+  constexpr auto create(auto& container) const noexcept -> auto {
+    if constexpr (arity == 1)
+      return SingleArgResolver{Resolver{container}};
+    else
+      return Resolver{container};
+  }
+};
 
 }  // namespace dink
--- src/dink/invoker_test.cpp
+++ src/dink/invoker_test.cpp
@@ -4,11 +4,55 @@
 
 #include "invoker.hpp"
 #include <dink/test.hpp>
+#include <concepts>
+#include <utility>
 
 namespace dink {
 namespace {
 
-//
+// ----------------------------------------------------------------------------
+// IndexedResolverFactory
+// ----------------------------------------------------------------------------
+
+struct IndexedResolverFactoryTest {
+  struct Container {};
+
+  struct Resolver {
+    Container& container;
+  };
+
+  struct SingleArgResolver {
+    Resolver resolver;
+  };
+
+  using Sut = IndexedResolverFactory<Resolver, SingleArgResolver>;
+
+  template <typename Expected, std::size_t arity, std::size_t index>
+  constexpr auto test_single() {
+    using Actual = decltype(std::declval<Sut>().template create<arity, index>(
+        std::declval<Container&>()));
+
+    static_assert(std::same_as<Expected, Actual>);
+  }
+
+  constexpr auto test_multiple() {
+    test_single<Resolver, 0, 0>();
+    test_single<Resolver, 0, 1>();
+    test_single<Resolver, 0, 2>();
+
+    test_single<SingleArgResolver, 1, 0>();
+    test_single<SingleArgResolver, 1, 1>();
+    test_single<SingleArgResolver, 1, 2>();
+
+    test_single<Resolver, 2, 0>();
+    test_single<Resolver, 2, 1>();
+    test_single<Resolver, 2, 2>();
+  }
+
+  constexpr IndexedResolverFactoryTest() { test_multiple(); }
+};
+[[maybe_unused]] constexpr auto indexed_resolver_factory_test =
+    IndexedResolverFactoryTest{};
 
 }  // namespace
 }  // namespace dink
