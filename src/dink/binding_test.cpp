a8904d181fb85dea084c2ff4c8382491db094859
Frank Secilia <frank.secilia@gmail.com>
2025-10-23 18:55:45 -0400

    feat(binding): shell module

---
+++ src/dink/binding_test.cpp
@@ -0,0 +1,12 @@
+// \file
+// Copyright (c) 2025 Frank Secilia
+// SPDX-License-Identifier: MIT
+
+#include "canonical.hpp"
+#include <dink/test.hpp>
+
+namespace dink {
+
+//
+
+}  // namespace dink
