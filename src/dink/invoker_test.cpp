ed6c2e912ccd7b0c74c2895d4fb4426adae620f6
Frank Secilia <frank.secilia@gmail.com>
2025-10-23 06:33:35 -0400

    test(invoker): factory tests multiple arities

--- src/dink/invoker_test.cpp
+++ src/dink/invoker_test.cpp
@@ -155,11 +155,30 @@ TEST_F(InvokerTestCtorRunTime, Arity3UniquePtr) {
 // ----------------------------------------------------------------------------
 
 struct InvokerFactoryTest {
-  struct Constructed {};
-  struct ConstructedFactory {
-    constexpr auto operator()() const noexcept -> Constructed;
-  };
   struct ResolverFactory {};
+
+  struct Constructed0 {
+    Constructed0();
+  };
+  struct Constructed1 {
+    Constructed1(int_t);
+  };
+  struct Constructed3 {
+    Constructed3(int_t, int_t, int_t);
+  };
+
+  struct ConstructedFactory0 {
+    constexpr auto operator()() const noexcept -> Constructed0;
+  };
+
+  struct ConstructedFactory1 {
+    constexpr auto operator()(int_t) const noexcept -> Constructed1;
+  };
+
+  struct ConstructedFactory3 {
+    constexpr auto operator()(int_t, int_t, int_t) const noexcept
+        -> Constructed3;
+  };
 
   template <typename Constructed, typename ConstructedFactory,
             typename ResolverFactory, typename>
@@ -167,6 +186,8 @@ struct InvokerFactoryTest {
 
   using Sut = InvokerFactory<Invoker>;
 
+  template <typename Constructed, typename ConstructedFactory,
+            std::size_t arity>
   static constexpr auto factory_specialization_result_type_matches() -> bool {
     using Actual =
         decltype(std::declval<Sut>()
@@ -174,22 +195,34 @@ struct InvokerFactoryTest {
                                       ResolverFactory>(
                          std::declval<ConstructedFactory>()));
     using Expected = Invoker<Constructed, ConstructedFactory, ResolverFactory,
-                             std::index_sequence<>>;
+                             std::make_index_sequence<arity>>;
     return std::same_as<Actual, Expected>;
   }
 
+  template <typename Constructed, std::size_t arity>
   static constexpr auto ctor_specialization_result_type_matches() -> bool {
     using Actual =
         decltype(std::declval<Sut>()
                      .template create<Constructed, ResolverFactory>());
-    using Expected =
-        Invoker<Constructed, void, ResolverFactory, std::index_sequence<>>;
+    using Expected = Invoker<Constructed, void, ResolverFactory,
+                             std::make_index_sequence<arity>>;
     return std::same_as<Actual, Expected>;
   }
 
   constexpr InvokerFactoryTest() {
-    static_assert(factory_specialization_result_type_matches());
-    static_assert(ctor_specialization_result_type_matches());
+    static_assert(
+        factory_specialization_result_type_matches<Constructed0,
+                                                   ConstructedFactory0, 0>());
+    static_assert(
+        factory_specialization_result_type_matches<Constructed1,
+                                                   ConstructedFactory1, 1>());
+    static_assert(
+        factory_specialization_result_type_matches<Constructed3,
+                                                   ConstructedFactory3, 3>());
+
+    static_assert(ctor_specialization_result_type_matches<Constructed0, 0>());
+    static_assert(ctor_specialization_result_type_matches<Constructed1, 1>());
+    static_assert(ctor_specialization_result_type_matches<Constructed3, 3>());
   }
 };
 [[maybe_unused]] constexpr auto invoker_factory_test = InvokerFactoryTest{};
