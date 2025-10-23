2ac17811c45a2a84d09f0425a29a775a613c0768
Frank Secilia <frank.secilia@gmail.com>
2025-10-22 20:17:56 -0400

    feat(provider): shell module

---
+++ src/dink/provider_test.cpp
@@ -0,0 +1,14 @@
+// \file
+// Copyright (c) 2025 Frank Secilia
+// SPDX-License-Identifier: MIT
+
+#include "provider.hpp"
+#include <dink/test.hpp>
+
+namespace dink {
+namespace {
+
+//
+
+}  // namespace
+}  // namespace dink
