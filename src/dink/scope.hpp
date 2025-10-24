8102d070818f6a44a32f6efae4d0c21bb72328fb
Frank Secilia <frank.secilia@gmail.com>
2025-10-23 21:18:51 -0400

    docs(scope): bash stale comments
    This promotion/relegation still happens, but not at this level anymore.

--- src/dink/scope.hpp
+++ src/dink/scope.hpp
@@ -9,24 +9,11 @@
 
 namespace dink::scope {
 
-//! nominally resolves new instances per request
-//
-// Instances resolved from transient scope are normally created per request,
-// meaning a unique instance is constructed from a provider for every request,
-// and the instance is returned with value semantics. A request for a transient
-// instance can be promoted to singleton scope if the type of the request
-// requires it, such as requesting a reference or pointer.
+//! resolves new instances per request
 struct Transient {
 };
 
-//! nominally resolves shared instances per request
-//
-// Instances resolved from singleton scope are normally cached by the
-// requesting container, meaning they are constructed from a provider once,
-// saved in the local cache, and the cached instance is returned with reference
-// semantics. A request for a singleton instance can be relegated to transient
-// scope if the type of the request requires it, such as requesting a
-// unique_ptr or rvalue reference.
+//! resolves shared instances across requests
 struct Singleton {
 };
 
