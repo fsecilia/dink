afbfc0d4efbd2801aef01263ffe899b2201ed73d
Frank Secilia <frank.secilia@gmail.com>
2025-10-23 21:20:06 -0400

    feat(scope): shell test module

---
+++ src/dink/scope_test.cpp
@@ -0,0 +1,14 @@
+// \file
+// Copyright (c) 2025 Frank Secilia
+// SPDX-License-Identifier: MIT
+
+#include "scope.hpp"
+#include <dink/test.hpp>
+
+namespace dink::scope {
+namespace {
+
+//
+
+}  // namespace
+}  // namespace dink::scope
