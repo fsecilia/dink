2b91bfa51279d0993698cd057a9a7a23d163d045
Frank Secilia <frank.secilia@gmail.com>
2025-10-30 02:50:22 -0400

    test(dispatcher): test BindingLocator

--- src/dink/dispatcher_test.cpp
+++ src/dink/dispatcher_test.cpp
@@ -6,7 +6,31 @@
 #include <dink/test.hpp>
 
 namespace dink {
+namespace {
 
-//
+struct DefaultsBindingLocatorTest {
+  struct FromType {
+    int_t id{};
+  };
+  static constexpr auto expected_from = FromType{5};
 
+  struct Config {
+    template <typename FromType>
+    constexpr auto find_binding() const noexcept -> FromType {
+      return expected_from;
+    }
+  };
+  static constexpr Config config{};
+
+  using Sut = defaults::BindingLocator;
+  static constexpr Sut sut{};
+
+  constexpr DefaultsBindingLocatorTest() {
+    static_assert(expected_from.id == sut.template find<FromType>(config).id);
+  }
+};
+[[maybe_unused]] constexpr auto defaults_bindings_locator_test =
+    DefaultsBindingLocatorTest{};
+
+}  // namespace
 }  // namespace dink
